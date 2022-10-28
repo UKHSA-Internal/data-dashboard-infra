c2ef2347b7ea3875641545d954295406da3d4aa0
Alistair Masawi <alistair284@gmail.com>
2022-10-28 12:22:42 +0100

    update details

--- main.tf
+++ main.tf
@@ -14,3 +14,12 @@ provider "aws" {
   version = "~>3.0"
   region  = "eu-west-2"
 }
+
+required_version = ">=0.14.9" 
+
+   backend "s3" {
+       bucket = "ukhsa-dashboard-terra-backend"
+       key    = "wl-backend-s3"
+       region = "eu-west-2"
+   }
+}
