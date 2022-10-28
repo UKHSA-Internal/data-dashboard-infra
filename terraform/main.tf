e5c1a511573543643fd392ecb45f9499b86187c1
Alistair Masawi <alistairmasawi@capacitas.co.uk>
2022-10-28 14:31:09 +0100

    backend-tf-2

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,14 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      Version = "~>4.16"
+      version = "~>4.16"
     }
+  }
+
+  backend "s3" {
+    bucket = "ukhsa-dashboard-terra-backend"
+    key    = "wl-backend-s3"
+    region = "eu-west-2"
   }
 
   required_version = ">=1.2.0"
@@ -14,9 +20,5 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
-backend "s3" {
-    bucket = "ukhsa-dashboard-terra-backend"
-    key    = "wl-backend-s3"
-    region = "eu-west-2"
-}
+
 
