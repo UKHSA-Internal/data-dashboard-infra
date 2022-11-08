d01b3cf67d1c62f4770a9668cb1cb9881c8008d9
Alistair Masawi <alistairmasawi@capacitas.co.uk>
2022-11-08 20:46:01 +0000

    update policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -116,6 +116,6 @@ EOF
 
 
 resource "aws_iam_role_policy_attachment" "devops_github_actions_attach" {
-  role       = data.aws_iam_role.devops_github_actions.id
+  role       = data.aws_iam_role.devops_github_actions.name
   policy_arn = aws_iam_policy.devops_github_actions_policy.arn
 }
