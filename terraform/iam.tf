a7d1de6f7941cc28dbd4aa3c765bc38b8212536a
Alistair Masawi <alistairmasawi@capacitas.co.uk>
2022-11-08 15:18:49 +0000

    add policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,24 +1,24 @@
 # # IAM roles
-resource "aws_iam_role" "app_execution_role" {
-  name               = "${var.project_name}-execution-role-1"
-  assume_role_policy = <<EOF
-{
-  "Version": "2008-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": [
-          "ecs.amazonaws.com",
-          "ecs-tasks.amazonaws.com"
-        ]
-      },
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
+#resource "aws_iam_role" "app_execution_role" {
+#  name               = "${var.project_name}-execution-role-1"
+#  assume_role_policy = <<EOF
+#{
+#  "Version": "2008-10-17",
+#  "Statement": [
+#    {
+#      "Action": "sts:AssumeRole",
+#      "Principal": {
+#        "Service": [
+#          "ecs.amazonaws.com",
+#          "ecs-tasks.amazonaws.com"
+#        ]
+#      },
+#      "Effect": "Allow"
+#    }
+#  ]
+#}
+#EOF
+#}
 
 resource "aws_iam_role" "role" {
   name = "devops_github_actions_wp_api"
