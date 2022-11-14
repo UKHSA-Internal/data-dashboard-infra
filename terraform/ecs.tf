46f25619187a1c29fc6a979f87f515cf97c457be
Alistair Masawi <alistairmasawi@capacitas.co.uk>
2022-11-14 15:28:40 +0000

    ecs policy

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -49,16 +49,5 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_ecs_service" "wp_api_service" {
-  name            = "wp-api-service"                             # Naming our first service
-  cluster         = "${aws_ecs_cluster.wp_api_cluster.id}"             # Referencing our created Cluster
-  task_definition = "${aws_ecs_task_definition.wp_api_task.arn}" # Referencing the task our service will spin up
-  launch_type     = "FARGATE"
-  desired_count   = 3 # Setting the number of containers we want deployed to 3
-  
-   network_configuration {
-    subnets          = [var.subnet_id_1,var.subnet_id_2,var.subnet_id_3]
-    assign_public_ip = true # Providing our containers with public IPs
-  }
-}
+
 
