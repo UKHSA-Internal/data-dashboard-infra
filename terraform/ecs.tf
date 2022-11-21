734ef4baa3d5b4b4516e6faa60a81fdc14de4129
Alistair Masawi <alistair.masawi@ukhsa.gov.uk>
2022-11-21 17:18:58 +0000

    load balancer to ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -99,10 +99,17 @@ resource "aws_ecs_service" "wp_api_service" {
 resource "aws_ecs_service" "wp_frontend_service" {
   name            = "wp-frontend-service"                             # Naming our first service
   cluster         = "${aws_ecs_cluster.wp_frontend_cluster.id}"             # Referencing our created Cluster
-  task_definition = "${aws_ecs_task_definition.wp_api_task.arn}" # Referencing the task our service will spin up
+  task_definition = "${aws_ecs_task_definition.wp_api_frontend_task.arn}" # Referencing the task our service will spin up
   launch_type     = "FARGATE"
   desired_count   = 3 # Setting the number of containers we want deployed to 3
-   network_configuration {
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.wp_target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.wp_api_frontend_task.family}"
+    container_port   = 3000 # Specifying the container port
+  }
+
+  network_configuration {
     subnets          = [var.subnet_id_1,var.subnet_id_2,var.subnet_id_3]
     assign_public_ip = true # Providing our containers with public IPs
   }
