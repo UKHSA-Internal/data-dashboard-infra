6b4397dbf5f4d02c14681c69b4e0224a10b6104f
Alistair Masawi <alistair.masawi@ukhsa.gov.uk>
2022-11-21 11:49:21 +0000

    frontend role added 1

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role" "app_execution_role" {
 EOF
 }
 
-resource "aws_iam_role" "api_role" {
+resource "aws_iam_role" "role" {
   name = "devops_github_actions_wp_api"
 
   assume_role_policy = <<EOF
@@ -44,7 +44,7 @@ resource "aws_iam_role" "api_role" {
 EOF
 }
 
-resource "aws_iam_policy" "api_policy" {
+resource "aws_iam_policy" "policy" {
   name        = "devops_github_actions_wp_api_policy"
   description = "A test policy"
 
@@ -117,9 +117,9 @@ resource "aws_iam_policy" "frontend_policy" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "api_attach" {
-  role       = aws_iam_role.api_role.name
-  policy_arn = aws_iam_policy.api_policy.arn
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "frontend_attach" {
