d7478d9e5bc64b320026e732a1293b0efd7233e4
Alistair Masawi <alistair.masawi@ukhsa.gov.uk>
2022-11-22 10:52:49 +0000

    ecs security group

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -115,3 +115,19 @@ resource "aws_ecs_service" "wp_frontend_service" {
   }
 }
 
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0 # Allowing any incoming port
+    to_port     = 0 # Allowing any outgoing port
+    protocol    = "-1" # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
