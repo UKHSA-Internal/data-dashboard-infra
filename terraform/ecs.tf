7c4a7d7902142a04d854cc317129e8dcbe4f575b
Alistair Masawi <alistair.masawi@ukhsa.gov.uk>
2022-12-01 16:09:04 +0000

    remove api cluster

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -7,31 +7,31 @@ resource "aws_ecs_cluster" "wp_frontend_cluster" {
 }
 
 
-resource "aws_ecs_task_definition" "wp_api_task" {
-  family                   = "wp-api-task" # Naming our first task
-  container_definitions    = <<DEFINITION
-  [
-    {
-      "name": "wp-api-task",
-      "image": "${aws_ecr_repository.ecr_repository_api.repository_url}",
-      "essential": true,
-      "portMappings": [
-        {
-          "containerPort": 80,
-          "hostPort": 80
-        }
-      ],
-      "memory": 512,
-      "cpu": 256
-    }
-  ]
-  DEFINITION
-  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
-  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-  memory                   = 512         # Specifying the memory our container requires
-  cpu                      = 256         # Specifying the CPU our container requires
-  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
-}
+#resource "aws_ecs_task_definition" "wp_api_task" {
+#  family                   = "wp-api-task" # Naming our first task
+#  container_definitions    = <<DEFINITION
+#  [
+#    {
+#      "name": "wp-api-task",
+#      "image": "${aws_ecr_repository.ecr_repository_api.repository_url}",
+#      "essential": true,
+#      "portMappings": [
+#        {
+#          "containerPort": 80,
+#          "hostPort": 80
+#        }
+#      ],
+#      "memory": 512,
+#      "cpu": 256
+#    }
+#  ]
+#  DEFINITION
+#  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+#  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+#  memory                   = 512         # Specifying the memory our container requires
+#  cpu                      = 256         # Specifying the CPU our container requires
+#  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+#}
 
 resource "aws_ecs_task_definition" "wp_api_frontend_task" {
   family                   = "wp-frontend-task" # Naming our first task
@@ -84,25 +84,25 @@ resource "aws_iam_service_linked_role" "ecs" {
   aws_service_name = "ecs.amazonaws.com"
 }
 
-resource "aws_ecs_service" "wp_api_service" {
-  name            = "wp-api-service-1"                             # Naming our first service
-  cluster         = "${aws_ecs_cluster.wp_api_cluster.id}"             # Referencing our created Cluster
-  task_definition = "${aws_ecs_task_definition.wp_api_task.arn}" # Referencing the task our service will spin up
-  launch_type     = "FARGATE"
-  desired_count   = 3 # Setting the number of containers we want deployed to 3
-
-  load_balancer {
-    target_group_arn = "${aws_lb_target_group.wp_api_target_group.arn}" # Referencing our target group
-    container_name   = "${aws_ecs_task_definition.wp_api_task.family}"
-    container_port   = 80 #------80  Specifying the container port
-  }
-   
-  network_configuration {
-    subnets          = [var.subnet_id_1,var.subnet_id_2,var.subnet_id_3]#[var.subnet_id_5,var.subnet_id_6,var.subnet_id_7]
-    assign_public_ip = true # Providing our containers with public IPs
-    security_groups  = ["${aws_security_group.service_security_group.id}"]
-  }
-}
+#resource "aws_ecs_service" "wp_api_service" {
+#  name            = "wp-api-service-1"                             # Naming our first service
+#  cluster         = "${aws_ecs_cluster.wp_api_cluster.id}"             # Referencing our created Cluster
+#  task_definition = "${aws_ecs_task_definition.wp_api_task.arn}" # Referencing the task our service will spin up
+#  launch_type     = "FARGATE"
+#  desired_count   = 3 # Setting the number of containers we want deployed to 3
+#
+#  load_balancer {
+#    target_group_arn = "${aws_lb_target_group.wp_api_target_group.arn}" # Referencing our target group
+#    container_name   = "${aws_ecs_task_definition.wp_api_task.family}"
+#    container_port   = 80 #------80  Specifying the container port
+#  }
+#   
+#  network_configuration {
+#    subnets          = [var.subnet_id_1,var.subnet_id_2,var.subnet_id_3]#[var.subnet_id_5,var.subnet_id_6,var.subnet_id_7]
+#    assign_public_ip = true # Providing our containers with public IPs
+#    security_groups  = ["${aws_security_group.service_security_group.id}"]
+#  }
+#}
 
 resource "aws_ecs_service" "wp_frontend_service" {
   name            = "wp-frontend-service"                             # Naming our first service
--- terraform/lb.tf
+++ terraform/lb.tf
@@ -6,13 +6,14 @@ resource "aws_alb" "wp_application_load_balancer" {
   security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
 }
 
-resource "aws_alb" "wp_application_load_balancer_api" {
-  name               = "wp-lb-api" # Naming our load balancer
-  load_balancer_type = "application"
-  subnets = [var.subnet_id_5,var.subnet_id_6,var.subnet_id_7]
-  # Referencing the security group
-  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-}
+#resource "aws_alb" "wp_application_load_balancer_api" {
+#  name               = "wp-lb-api" # Naming our load balancer
+#  load_balancer_type = "application"
+#  subnets = [var.subnet_id_5,var.subnet_id_6,var.subnet_id_7]
+#  # Referencing the security group
+#  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+#}
+
 # Creating a security group for the load balancer:
 resource "aws_security_group" "load_balancer_security_group" {
   vpc_id      = var.vpc_id
@@ -44,18 +45,18 @@ resource "aws_lb_target_group" "wp_target_group" {
   }
 }
 
-resource "aws_lb_target_group" "wp_api_target_group" {
-  name        = "wp-api-target-group"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = var.vpc_id # Referencing the default VPC
-  health_check {
-    matcher = "200,301,302"
-    path = "/"
-    interval = 70
-  }
-}
+#resource "aws_lb_target_group" "wp_api_target_group" {
+#  name        = "wp-api-target-group"
+#  port        = 80
+#  protocol    = "HTTP"
+#  target_type = "ip"
+#  vpc_id      = var.vpc_id # Referencing the default VPC
+#  health_check {
+#    matcher = "200,301,302"
+#    path = "/"
+#    interval = 70
+#  }
+#}
 
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = "${aws_alb.wp_application_load_balancer.arn}" # Referencing our load balancer
@@ -68,13 +69,13 @@ resource "aws_lb_listener" "listener" {
   }
 }
 
-resource "aws_lb_listener" "api_listener" {
-  load_balancer_arn = "${aws_alb.wp_application_load_balancer_api.arn}" # Referencing our load balancer
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.wp_api_target_group.arn}" # Referencing our tagrte group
-  }
-}
+#resource "aws_lb_listener" "api_listener" {
+#  load_balancer_arn = "${aws_alb.wp_application_load_balancer_api.arn}" # Referencing our load balancer
+#  port              = "80"
+#  protocol          = "HTTP"
+#
+#  default_action {
+#    type             = "forward"
+#    target_group_arn = "${aws_lb_target_group.wp_api_target_group.arn}" # Referencing our tagrte group
+#  }
+#}
