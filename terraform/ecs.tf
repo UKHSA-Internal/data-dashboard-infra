4459b771e74598fa7eed64acca4f236965551985
Alistair Masawi <alistair.masawi@ukhsa.gov.uk>
2022-12-01 16:28:37 +0000

    add API lb and service

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -84,25 +84,25 @@ resource "aws_iam_service_linked_role" "ecs" {
   aws_service_name = "ecs.amazonaws.com"
 }
 
-#resource "aws_ecs_service" "wp_api_service" {
-#  name            = "wp-api-service-1"                             # Naming our first service
-#  cluster         = "${aws_ecs_cluster.wp_api_cluster.id}"             # Referencing our created Cluster
-#  task_definition = "${aws_ecs_task_definition.wp_api_task.arn}" # Referencing the task our service will spin up
-#  launch_type     = "FARGATE"
-#  desired_count   = 3 # Setting the number of containers we want deployed to 3
-#
-#  load_balancer {
-#    target_group_arn = "${aws_lb_target_group.wp_api_target_group.arn}" # Referencing our target group
-#    container_name   = "${aws_ecs_task_definition.wp_api_task.family}"
-#    container_port   = 80 #------80  Specifying the container port
-#  }
-#   
-#  network_configuration {
-#    subnets          = [var.subnet_id_1,var.subnet_id_2,var.subnet_id_3]#[var.subnet_id_5,var.subnet_id_6,var.subnet_id_7]
-#    assign_public_ip = true # Providing our containers with public IPs
-#    security_groups  = ["${aws_security_group.service_security_group.id}"]
-#  }
-#}
+resource "aws_ecs_service" "wp_api_service" {
+  name            = "wp-api-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.wp_api_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.wp_api_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.wp_api_target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.wp_api_task.family}"
+    container_port   = 80 #------80  Specifying the container port
+  }
+  
+  network_configuration {
+    subnets          = [var.subnet_id_5,var.subnet_id_6,var.subnet_id_7]
+    assign_public_ip = true # Providing our containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group_api.id}"]
+  }
+}
 
 resource "aws_ecs_service" "wp_frontend_service" {
   name            = "wp-frontend-service"                             # Naming our first service
@@ -132,6 +132,24 @@ resource "aws_security_group" "service_security_group" {
     protocol  = "-1"
     # Only allowing traffic in from the load balancer security group
     security_groups =["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0 # Allowing any incoming port
+    to_port     = 0 # Allowing any outgoing port
+    protocol    = "-1" # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
+
+resource "aws_security_group" "service_security_group_api" {
+  vpc_id      = var.vpc_id
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups =["${aws_security_group.load_balancer_api_security_group.id}"]
   }
 
   egress {
