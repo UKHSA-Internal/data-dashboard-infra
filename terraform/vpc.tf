88a58c66a8a5763fc294ede844c15a7668fac70a
Ben Collier <bencollier@fastmail.com>
2022-12-05 17:03:39 +0000

    Add internet gateway for S3.

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -41,3 +41,61 @@ resource "aws_db_subnet_group" "default" {
   subnet_ids  = [var.subnet_id_1,var.subnet_id_2,var.subnet_id_3,var.subnet_id_4,var.subnet_id_5,var.subnet_id_6,var.subnet_id_7]
 }
 
+resource "aws_internet_gateway" "s3_gateway" {
+  vpc_id = var.vpc_id
+}
+
+resource aws_route_table "s3_route_table" {
+  vpc_id = var.vpc_id
+}
+
+resource aws_route "s3_route" {
+  route_table_id = aws_route_table.s3_route_table
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.s3_gateway
+}
+
+resource aws_route_table_association "s3_route_table_association_1" {
+  subnet_id = data.aws_subnet.subnet_1
+  route_table_id = aws_route_table.s3_route_table
+}
+
+resource aws_route_table_association "s3_route_table_association_2" {
+  subnet_id = data.aws_subnet.subnet_2
+  route_table_id = aws_route_table.s3_route_table
+}
+
+resource aws_route_table_association "s3_route_table_association_3" {
+  subnet_id = data.aws_subnet.subnet_3
+  route_table_id = aws_route_table.s3_route_table
+}
+
+resource aws_route_table_association "s3_route_table_association_4" {
+  subnet_id = data.aws_subnet.subnet_4
+  route_table_id = aws_route_table.s3_route_table
+}
+
+resource aws_route_table_association "s3_route_table_association_5" {
+  subnet_id = data.aws_subnet.subnet_5
+  route_table_id = aws_route_table.s3_route_table
+}
+
+resource aws_route_table_association "s3_route_table_association_6" {
+  subnet_id = data.aws_subnet.subnet_6
+  route_table_id = aws_route_table.s3_route_table
+}
+
+resource aws_route_table_association "s3_route_table_association_7" {
+  subnet_id = data.aws_subnet.subnet_7
+  route_table_id = aws_route_table.s3_route_table
+}
+
+resource aws_vpc_endpoint "s3_endpoint" {
+  vpc_id = var.vpc_id
+  service_name = "com.amazonaws.eu-west2.s3"
+}
+
+resource aws_vpc_endpoint_route_table_association "s3_endpoint_route_table_association" {
+  route_table_id = aws_route_table.s3_route_table
+  vpc_endpoint_id = aws_vpc_endpoint.s3_endpoint
+}
