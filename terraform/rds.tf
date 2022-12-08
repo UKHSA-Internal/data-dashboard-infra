66202b6143bff73b94ebe5cb4618cfd2d6b1c858
Alistair Masawi <alistair.masawi@ukhsa.gov.uk>
2022-12-08 09:12:37 +0000

    Add rds, ecr and vpc

---
+++ terraform/rds.tf
@@ -0,0 +1,23 @@
+# Postgres database RDS
+data "aws_secretsmanager_secret" "secrets" {
+  arn = "arn:aws:secretsmanager:eu-west-2:518944279943:secret:rds_credentials-CsBKKO"
+}
+
+data "aws_secretsmanager_secret_version" "current" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+resource "aws_db_instance" "app_rds" {
+  identifier                = "${var.project_name}-rds"
+  db_name                   = var.rds_db_name
+  allocated_storage         = var.rds_allocated_storage
+  engine                    = var.rds_engine
+  engine_version            = var.rds_engine_version
+  instance_class            = var.rds_instance_class
+  username                  = "wp_user"
+  password                  = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["rds_password"]
+  storage_type              = var.rds_storage_type
+  db_subnet_group_name      = "${aws_db_subnet_group.default.id}"
+  skip_final_snapshot       = true
+}
+
---
+++ terraform/vpc.tf
@@ -0,0 +1,101 @@
+resource "aws_vpc" "wp_dev_vpc" {
+  cidr_block       = "10.10.144.0/20"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "subnet_1" {
+  vpc_id     = aws_vpc.wp_dev_vpc.id
+  cidr_block = "10.10.144.0/27"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id     = aws_vpc.wp_dev_vpc.id
+  cidr_block = "10.10.144.0/27"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subnet" "subnet_3" {
+  vpc_id     = aws_vpc.wp_dev_vpc.id
+  cidr_block = "10.10.144.0/27"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+
+resource "aws_db_subnet_group" "default" {
+  name        = "main"
+  description = "Terraform example RDS subnet group"
+  subnet_ids  = [aws_subnet.subnet_1.id,aws_subnet.subnet_2.id,aws_subnet.subnet_3.id]
+}
+
+
+
+
+/*
+
+
+data "aws_subnet" "subnet_1" {
+  id = var.subnet_id_1
+}
+
+data "aws_subnet" "subnet_2" {
+  id = var.subnet_id_2
+}
+
+data "aws_subnet" "subnet_3" {
+  id = var.subnet_id_3
+}
+
+data "aws_subnet" "subnet_4" {
+  id = var.subnet_id_4
+}
+
+data "aws_subnet" "subnet_5" {
+  id = var.subnet_id_5
+}
+
+data "aws_subnet" "subnet_6" {
+  id = var.subnet_id_6
+}
+
+data "aws_subnet" "subnet_7" {
+  id = var.subnet_id_7
+}
+
+data "aws_subnets" "app_subnets"{
+  filter{
+    name = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+resource "aws_db_subnet_group" "default" {
+  name        = "main1"
+  description = "Terraform example RDS subnet group"
+  subnet_ids  = [var.subnet_id_1,var.subnet_id_2,var.subnet_id_3,var.subnet_id_4,var.subnet_id_5,var.subnet_id_6,var.subnet_id_7]
+}
+
+resource aws_vpc_endpoint "s3_endpoint" {
+  vpc_id = var.vpc_id
+  service_name = "com.amazonaws.eu-west-2.s3"
+  route_table_ids = ["rtb-0cfb03a1ff2c0029c"]
+}
+*/
+
+
+
+
+
