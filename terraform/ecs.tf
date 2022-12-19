ed8ec25f99c8072c7347238300682769775e98ff
Ben Collier <bmcollier@gmail.com>
2022-12-19 19:42:33 +0000

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -105,17 +105,20 @@ resource "aws_ecs_task_definition" "wp_api_frontend_task" {
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "ecsTaskExecutionRole"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+  assume_role_policy = "${data.aws_iam_policy_document.access_s3_policy.json}"
 }
 
-data "aws_iam_policy_document" "assume_role_policy" {
+data "aws_iam_policy_document" "access_s3_policy" {
   statement {
-    actions = ["sts:AssumeRole"]
+    effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "arn:aws:s3:::wp-incoming-dev",         
+    ]
   }
 }
 
