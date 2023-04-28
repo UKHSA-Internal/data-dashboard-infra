d464da52a71ea0b2cb9ff3c15fd4e076e8881f64
Kevin Kuszyk <kevin.kuszyk@ukhsa.gov.uk>
2023-04-28 17:38:21 +0100

    CDD-732 Adding `terraform apply` script

--- scripts/_terraform.sh
+++ scripts/_terraform.sh
@@ -5,10 +5,11 @@ function _terraform_help() {
     echo "uhd terraform <command> [options]"
     echo
     echo "commands:"
-    echo "  help                           - this help screen"
+    echo "  help                             - this help screen"
     echo
-    echo "  init:layer <layer>             - runs `terraform init` for the specified layer" 
-    echo "  plan:layer <layer> <workspace> - runs `terraform plan` for the specified layer and workspace"
+    echo "  init:layer <layer>               - runs `terraform init` for the specified layer" 
+    echo "  plan:layer <layer> <workspace>   - runs `terraform plan` for the specified layer and workspace"
+    echo "  apply:layer <layer> <workspace>  - runs `terraform apply` for the specified layer and workspace"
     echo
 
     return 1
@@ -21,6 +22,7 @@ function _terraform() {
     case $verb in
         "init:layer") _terraform_init_layer $args ;;
         "plan:layer") _terraform_plan_layer $args ;;
+        "apply:layer") _terraform_apply_layer $args ;;
 
         *) _terraform_help ;;
     esac
@@ -67,6 +69,34 @@ function _terraform_plan_layer() {
 
     terraform plan \
         -var "assume_account_id=${assume_account_id}" 
+}
+
+function _terraform_apply_layer() {
+    local layer=$1
+    local workspace=$2
+
+    if [[ -z ${layer} ]]; then
+        echo "Layer is required" >&2
+        return 1
+    fi
+
+    if [[ -z ${workspace} ]]; then
+        echo "Workspace is required" >&2
+        return 1
+    fi
+
+    local terraform_dir=$(_get_terraform_dir $layer)
+
+    echo "Running terraform apply for layer '$layer' and workspace '$workspace'..."
+
+    cd $terraform_dir
+    terraform workspace select "$workspace" || terraform workspace new "$workspace" || return 1
+
+    local assume_account_id=$(_get_aws_account_id $workspace)
+
+    terraform apply \
+        -var "assume_account_id=${assume_account_id}" \
+        -auto-approve 
 }
 
 function _get_terraform_dir() {
