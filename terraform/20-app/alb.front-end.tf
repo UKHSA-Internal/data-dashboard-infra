5fb95db9c1b101a7f1427942166a9d95affbe499
Kevin Kuszyk <kevin.kuszyk@ukhsa.gov.uk>
2023-05-12 12:24:39 +0100

    CDD-755 Adding ecs services and lb for front end and api

---
+++ terraform/20-app/alb.front-end.tf
@@ -0,0 +1,67 @@
+module "front_end_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "${local.prefix}-front-end"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = [module.front_end_alb_security_group.security_group_id]
+
+  target_groups = [
+    {
+      name             = "${local.prefix}-front-end"
+      backend_protocol = "HTTP"
+      backend_port     = 3000
+      target_type      = "ip"
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 5
+        protocol            = "HTTP"
+        matcher             = "200,404"
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+}
+
+module "front_end_alb_security_group" {
+    source = "terraform-aws-modules/security-group/aws"
+
+    name   = "${local.prefix}-front-end-alb"
+    vpc_id = module.vpc.vpc_id
+
+    ingress_with_cidr_blocks = [
+        {
+            description     = "http from allowed ips"
+            rule            = "http-80-tcp"
+            cidr_blocks     = join(",", local.ip_allow_list.engineers, 
+                                        local.ip_allow_list.project_team, 
+                                        local.ip_allow_list.other_stakeholders)
+        }
+    ]
+
+    egress_with_source_security_group_id = [
+        {
+            description              = "lb to tasks"
+            from_port                = 3000
+            to_port                  = 3000
+            protocol                 = "TCP"
+            source_security_group_id = module.ecs.services[local.ecs.services.front_end].security_group_id 
+        }
+    ]
+}
