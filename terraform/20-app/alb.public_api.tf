7af8d472d19a283d931ae31ee5fc122134677370
A-Ashiq <afaan.ashiq@nate.tech>
2023-07-18 09:43:38 +0100

    Add load balancers for `cms-admin`, `private-api` & `public-api`

---
+++ terraform/20-app/alb.public_api.tf
@@ -0,0 +1,64 @@
+module "public_api_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "8.7.0"
+
+  name = "${local.prefix}-public-api"
+
+  load_balancer_type = "application"
+
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.public_subnets
+  security_groups = [module.public_api_alb_security_group.security_group_id]
+
+  target_groups = [
+    {
+      name             = "${local.prefix}-public-api"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "ip"
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/health/"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 5
+        protocol            = "HTTP"
+        matcher             = "200"
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+}
+
+module "public_api_alb_security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  
+  name   = "${local.prefix}-public-api-alb"
+  vpc_id = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      description = "http from internet"
+      rule        = "http-80-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_source_security_group_id = [
+    {
+      description              = "lb to tasks"
+      rule                     = "http-80-tcp"
+      source_security_group_id = module.ecs_service_public_api.security_group_id
+    }
+  ]
+}
