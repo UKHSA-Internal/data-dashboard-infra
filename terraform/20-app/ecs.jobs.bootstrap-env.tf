0e018c3794cbaafe4ed58ab2be735a8cde6d2c85
A-Ashiq <afaan.ashiq@nate.tech>
2023-07-18 14:04:32 +0100

    Point `bootstrap-env` job at private api

--- terraform/20-app/ecs.jobs.bootstrap-env.tf
+++ terraform/20-app/ecs.jobs.bootstrap-env.tf
@@ -4,8 +4,8 @@ resource "local_sensitive_file" "ecs_job_bootstrap_env" {
     admin_password    = random_password.api_admin_user_password.result
     api_key           = local.api_key
     cluster_arn       = module.ecs.cluster_arn
-    security_group_id = module.ecs_service_api.security_group_id
+    security_group_id = module.ecs_service_private_api.security_group_id
     subnet_ids        = module.vpc.public_subnets
-    task_arn          = module.ecs_service_api.task_definition_arn
+    task_arn          = module.ecs_service_private_api.task_definition_arn
   })
 }
