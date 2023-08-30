32e3ae14f44dd90d86a77a8568a1a422dd945953
Kevin Kuszyk <kevin.kuszyk@ukhsa.gov.uk>
2023-08-30 09:01:09 +0100

    CDD-731 Adding report viewer role

--- terraform/10-account/iam.sso-roles.tf
+++ terraform/10-account/iam.sso-roles.tf
@@ -8,9 +8,15 @@ data "aws_iam_roles" "developer" {
   path_prefix = "/aws-reserved/sso.amazonaws.com/"
 }
 
+data "aws_iam_roles" "report_viewer" {
+  name_regex  = "AWSReservedSSO_ReportViewer_*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
+
 locals {
   sso_role_arns = {
     administrator = tolist(data.aws_iam_roles.administrator.arns)[0]
     developer     = tolist(data.aws_iam_roles.developer.arns)[0]
+    report_viewer = tolist(data.aws_iam_roles.report_viewer.arns)[0]
   }
 }
