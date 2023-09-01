7be5d2f4eee82023c4d72d4a28d77390790c579d
Kevin Kuszyk <kevin.kuszyk@ukhsa.gov.uk>
2023-09-01 12:23:23 +0100

    CDD-1181 Adding zones for `pen`, `perf`, and `train` envs

--- terraform/10-account/outputs.tf
+++ terraform/10-account/outputs.tf
@@ -4,5 +4,19 @@ output "dns" {
       zone_id      = module.route_53_zone_account.route53_zone_zone_id[var.account_dns_name]
       name_servers = module.route_53_zone_account.route53_zone_name_servers[var.account_dns_name]
     }
+    wke = {
+      pen = local.account == "test" ? {
+        zone_id      = module.route_53_zone_wke_test_account.route53_zone_zone_id[local.wke_dns_names.pen]
+        name_servers = module.route_53_zone_wke_test_account.route53_zone_name_servers[local.wke_dns_names.pen]
+      } : null
+      perf = local.account == "test" ? {
+        zone_id      = module.route_53_zone_wke_test_account.route53_zone_zone_id[local.wke_dns_names.perf]
+        name_servers = module.route_53_zone_wke_test_account.route53_zone_name_servers[local.wke_dns_names.perf]
+      } : null
+      train = local.account == "uat" ? {
+        zone_id      = module.route_53_zone_wke_uat_account.route53_zone_zone_id[local.wke_dns_names.train]
+        name_servers = module.route_53_zone_wke_uat_account.route53_zone_name_servers[local.wke_dns_names.train]
+      } : null
+    }
   }
 }
