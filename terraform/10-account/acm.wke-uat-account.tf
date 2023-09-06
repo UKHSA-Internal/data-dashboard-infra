acb4dbff42001f050880867929d4be5713c00522
Kevin Kuszyk <kevin.kuszyk@ukhsa.gov.uk>
2023-09-06 08:44:53 +0100

    CDD-813 Fix typo to all creating certs in uat account

--- terraform/10-account/acm.wke-uat-account.tf
+++ terraform/10-account/acm.wke-uat-account.tf
@@ -5,7 +5,7 @@ module "acm_wke_train" {
   create_certificate = local.account == "uat"
 
   domain_name = local.wke_dns_names.train
-  zone_id     = local.account == "uat" ? module.route_53_zone_wke_test_account.route53_zone_zone_id[local.wke_dns_names.train] : ""
+  zone_id     = local.account == "uat" ? module.route_53_zone_wke_uat_account.route53_zone_zone_id[local.wke_dns_names.train] : ""
 
   subject_alternative_names = [
     "*.${local.wke_dns_names.train}"
