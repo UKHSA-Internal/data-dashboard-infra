5ca02491fd4db1b7fec059a0610225f0f149a7d1
Kevin Kuszyk <kevin.kuszyk@ukhsa.gov.uk>
2023-09-14 20:19:18 +0100

    CDD-1268 Adding sql to create athena tables for waf logs

---
+++ athena/tables/public-api-waf-logs.sql
@@ -0,0 +1,137 @@
+/* 
+ * Athena doesn't support sql variables, so you'll need to find and replace the following tokens before running:
+ *  - {ENV NAME}      => environment name
+ *  - {ACCOUNT ID}    => aws account id you are deploying into
+ *  - {HALO ENV TYPE} => the HALO environment type -nl1, nl2, etc
+ */
+
+CREATE EXTERNAL TABLE `uhd-{ENV NAME}-public-api-waf-logs`(
+  `timestamp` bigint,
+  `formatversion` int,
+  `webaclid` string,
+  `terminatingruleid` string,
+  `terminatingruletype` string,
+  `action` string,
+  `terminatingrulematchdetails` array <
+                                    struct <
+                                        conditiontype: string,
+                                        sensitivitylevel: string,
+                                        location: string,
+                                        matcheddata: array < string >
+                                          >
+                                     >,
+  `httpsourcename` string,
+  `httpsourceid` string,
+  `rulegrouplist` array <
+                      struct <
+                          rulegroupid: string,
+                          terminatingrule: struct <
+                                              ruleid: string,
+                                              action: string,
+                                              rulematchdetails: array <
+                                                                   struct <
+                                                                       conditiontype: string,
+                                                                       sensitivitylevel: string,
+                                                                       location: string,
+                                                                       matcheddata: array < string >
+                                                                          >
+                                                                    >
+                                                >,
+                          nonterminatingmatchingrules: array <
+                                                              struct <
+                                                                  ruleid: string,
+                                                                  action: string,
+                                                                  overriddenaction: string,
+                                                                  rulematchdetails: array <
+                                                                                       struct <
+                                                                                           conditiontype: string,
+                                                                                           sensitivitylevel: string,
+                                                                                           location: string,
+                                                                                           matcheddata: array < string >
+                                                                                              >
+                                                                                       >
+                                                                    >
+                                                             >,
+                          excludedrules: string
+                            >
+                       >,
+`ratebasedrulelist` array <
+                         struct <
+                             ratebasedruleid: string,
+                             limitkey: string,
+                             maxrateallowed: int
+                               >
+                          >,
+  `nonterminatingmatchingrules` array <
+                                    struct <
+                                        ruleid: string,
+                                        action: string,
+                                        rulematchdetails: array <
+                                                             struct <
+                                                                 conditiontype: string,
+                                                                 sensitivitylevel: string,
+                                                                 location: string,
+                                                                 matcheddata: array < string >
+                                                                    >
+                                                             >,
+                                        captcharesponse: struct <
+                                                            responsecode: string,
+                                                            solvetimestamp: string
+                                                             >
+                                          >
+                                     >,
+  `requestheadersinserted` array <
+                                struct <
+                                    name: string,
+                                    value: string
+                                      >
+                                 >,
+  `responsecodesent` string,
+  `httprequest` struct <
+                    clientip: string,
+                    country: string,
+                    headers: array <
+                                struct <
+                                    name: string,
+                                    value: string
+                                      >
+                                 >,
+                    uri: string,
+                    args: string,
+                    httpversion: string,
+                    httpmethod: string,
+                    requestid: string
+                      >,
+  `labels` array <
+               struct <
+                   name: string
+                     >
+                >,
+  `captcharesponse` struct <
+                        responsecode: string,
+                        solvetimestamp: string,
+                        failureReason: string
+                          >
+)
+PARTITIONED BY ( 
+`region` string, 
+`date` string) 
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://aws-waf-logs-halo-{ACCOUNT ID}-us-east-1-wpr-{HALO ENV TYPE}-avm-bs-gr-r/AWSLogs/{ACCOUNT ID}/WAFLogs/cloudfront/uhd-{ENV NAME}-public-api/'
+TBLPROPERTIES(
+ 'projection.enabled' = 'true',
+ 'projection.region.type' = 'enum',
+ 'projection.region.values' = 'us-east-1,eu-west-2,cloudfront',
+ 'projection.date.type' = 'date',
+ 'projection.date.range' = '2021/01/01,NOW',
+ 'projection.date.format' = 'yyyy/MM/dd',
+ 'projection.date.interval' = '1',
+ 'projection.date.interval.unit' = 'DAYS',
+ 'storage.location.template' = 's3://aws-waf-logs-halo-{ACCOUNT ID}-us-east-1-wpr-{HALO ENV TYPE}-avm-bs-gr-r/AWSLogs/{ACCOUNT ID}/WAFLogs/${region}/uhd-{ENV NAME}-public-api/${date}/') 
+ 
