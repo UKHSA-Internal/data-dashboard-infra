d7f61cb376b292790cb30483e7ee73d63efb720c
Kevin Kuszyk <kevin.kuszyk@ukhsa.gov.uk>
2023-09-15 12:29:33 +0100

    CDD-1269 Adding cloud front policies for caching and origin request

--- terraform/20-app/cloud-front.front-end.tf
+++ terraform/20-app/cloud-front.front-end.tf
@@ -1,46 +1,83 @@
 module "cloudfront_front_end" {
-    source                = "terraform-aws-modules/cloudfront/aws"
-    version               = "3.2.1"
+  source  = "terraform-aws-modules/cloudfront/aws"
+  version = "3.2.1"
 
-    comment               = "${local.prefix}-front-end"
-    enabled               = true
-    wait_for_deployment   = true
-    aliases               = [local.dns_names.front_end]
+  comment             = "${local.prefix}-front-end"
+  enabled             = true
+  wait_for_deployment = true
+  aliases             = [local.dns_names.front_end]
 
-    web_acl_id            = aws_wafv2_web_acl.front_end.arn
-  
-    origin = {
-        alb = {
-            domain_name            = local.dns_names.front_end_lb
-            custom_origin_config   = {
-                http_port     = 80
-                https_port    = 443
-                origin_protocol_policy = "https-only"
-                origin_ssl_protocols   = ["TLSv1.2"]
-            }
+  web_acl_id = aws_wafv2_web_acl.front_end.arn
 
-            custom_header = [
-                {
-                    name    = "x-cdn-auth"
-                    value   = jsonencode(aws_secretsmanager_secret_version.cdn_front_end_secure_header_value.secret_string)
-                }
-            ]
+  origin = {
+    alb = {
+      domain_name = local.dns_names.front_end_lb
+      custom_origin_config = {
+        http_port              = 80
+        https_port             = 443
+        origin_protocol_policy = "https-only"
+        origin_ssl_protocols   = ["TLSv1.2"]
+      }
+
+      custom_header = [
+        {
+          name  = "x-cdn-auth"
+          value = jsonencode(aws_secretsmanager_secret_version.cdn_front_end_secure_header_value.secret_string)
         }
+      ]
     }
+  }
 
-    viewer_certificate = {
-        acm_certificate_arn = local.cloud_front_certificate_arn
-        ssl_support_method  = "sni-only"
-        minimum_protocol_version = "TLSv1.2_2021"
-    }
+  viewer_certificate = {
+    acm_certificate_arn      = local.cloud_front_certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
 
-    default_cache_behavior = {
-        target_origin_id          = "alb"
-        viewer_protocol_policy    = "allow-all"
-        allowed_methods           = ["GET", "HEAD", "OPTIONS"]
-        cached_methods            = ["GET", "HEAD"]
-        min_ttl                   = local.environment == "prod" ? 2592000 : 900
-        default_ttl               = local.environment == "prod" ? 2592000 : 900
-        max_ttl                   = local.environment == "prod" ? 2592000 : 900
+  default_cache_behavior = {
+    allowed_methods          = ["GET", "HEAD", "OPTIONS"]
+    cache_policy_id          = aws_cloudfront_cache_policy.front_end.id
+    cached_methods           = ["GET", "HEAD"]
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.front_end.id
+    target_origin_id         = "alb"
+    use_forwarded_values     = false
+    viewer_protocol_policy   = "allow-all"
+  }
+}
+
+resource "aws_cloudfront_origin_request_policy" "front_end" {
+  name = "${local.prefix}-front-end"
+
+  cookies_config {
+    cookie_behavior = "none"
+  }
+  headers_config {
+    header_behavior = "allViewer"
+  }
+  query_strings_config {
+    query_string_behavior = "all"
+  }
+}
+
+resource "aws_cloudfront_cache_policy" "front_end" {
+  name = "${local.prefix}-front-end"
+
+  min_ttl     = local.environment == "prod" ? 2592000 : 900
+  default_ttl = local.environment == "prod" ? 2592000 : 900
+  max_ttl     = local.environment == "prod" ? 2592000 : 900
+
+  parameters_in_cache_key_and_forwarded_to_origin {
+    enable_accept_encoding_brotli = true
+    enable_accept_encoding_gzip   = true
+
+    cookies_config {
+      cookie_behavior = "none"
     }
+    headers_config {
+      header_behavior = "none"
+    }
+    query_strings_config {
+      query_string_behavior = "all"
+    }
+  }
 }
--- terraform/20-app/cloud-front.public-api.tf
+++ terraform/20-app/cloud-front.public-api.tf
@@ -1,50 +1,87 @@
 module "cloudfront_public_api" {
-    source                = "terraform-aws-modules/cloudfront/aws"
-    version               = "3.2.0"
+  source  = "terraform-aws-modules/cloudfront/aws"
+  version = "3.2.0"
 
-    comment               = "${local.prefix}-public-api"
-    enabled               = true
-    wait_for_deployment   = true
-    aliases               = [local.dns_names.public_api]
+  comment             = "${local.prefix}-public-api"
+  enabled             = true
+  wait_for_deployment = true
+  aliases             = [local.dns_names.public_api]
 
-    web_acl_id            = aws_wafv2_web_acl.public_api.arn
+  web_acl_id = aws_wafv2_web_acl.public_api.arn
 
-    origin = {
-        alb = {
-            domain_name          = local.dns_names.public_api_lb
-            custom_origin_config = {
-                http_port     = 80
-                https_port    = 443
-                origin_protocol_policy    = "https-only"
-                origin_ssl_protocols      = ["TLSv1.2"]
-            }
+  origin = {
+    alb = {
+      domain_name = local.dns_names.public_api_lb
+      custom_origin_config = {
+        http_port              = 80
+        https_port             = 443
+        origin_protocol_policy = "https-only"
+        origin_ssl_protocols   = ["TLSv1.2"]
+      }
 
-            custom_header = [
-                {
-                    name  = "Accept"
-                    value = "text/html"
-                },
-                {
-                    name    = "x-cdn-auth"
-                    value   = jsonencode(aws_secretsmanager_secret_version.cdn_public_api_secure_header_value.secret_string)
-                }
-            ]
+      custom_header = [
+        {
+          name  = "Accept"
+          value = "text/html"
+        },
+        {
+          name  = "x-cdn-auth"
+          value = jsonencode(aws_secretsmanager_secret_version.cdn_public_api_secure_header_value.secret_string)
         }
+      ]
     }
+  }
 
-    viewer_certificate = {
-        acm_certificate_arn = local.cloud_front_certificate_arn
-        ssl_support_method  = "sni-only"
-        minimum_protocol_version = "TLSv1.2_2021"
-    }
+  viewer_certificate = {
+    acm_certificate_arn      = local.cloud_front_certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
 
-    default_cache_behavior = {
-        target_origin_id          = "alb"
-        viewer_protocol_policy    = "allow-all"
-        allowed_methods           = ["GET", "HEAD", "OPTIONS"]
-        cached_methods            = ["GET", "HEAD"]
-        min_ttl                   = local.environment == "prod" ? 2592000 : 900
-        default_ttl               = local.environment == "prod" ? 2592000 : 900
-        max_ttl                   = local.environment == "prod" ? 2592000 : 900
+  default_cache_behavior = {
+    allowed_methods          = ["GET", "HEAD", "OPTIONS"]
+    cache_policy_id          = aws_cloudfront_cache_policy.public_api.id
+    cached_methods           = ["GET", "HEAD"]
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.public_api.id
+    target_origin_id         = "alb"
+    use_forwarded_values     = false
+    viewer_protocol_policy   = "allow-all"
+  }
+}
+
+resource "aws_cloudfront_origin_request_policy" "public_api" {
+  name = "${local.prefix}-public-api"
+
+  cookies_config {
+    cookie_behavior = "none"
+  }
+  headers_config {
+    header_behavior = "allViewer"
+  }
+  query_strings_config {
+    query_string_behavior = "all"
+  }
+}
+
+resource "aws_cloudfront_cache_policy" "public_api" {
+  name = "${local.prefix}-public-api"
+
+  min_ttl     = local.environment == "prod" ? 2592000 : 900
+  default_ttl = local.environment == "prod" ? 2592000 : 900
+  max_ttl     = local.environment == "prod" ? 2592000 : 900
+
+  parameters_in_cache_key_and_forwarded_to_origin {
+    enable_accept_encoding_brotli = true
+    enable_accept_encoding_gzip   = true
+
+    cookies_config {
+      cookie_behavior = "none"
     }
+    headers_config {
+      header_behavior = "none"
+    }
+    query_strings_config {
+      query_string_behavior = "all"
+    }
+  }
 }
