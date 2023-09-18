1fd1aff8b863102c556a6859016cdaa9b625013b
Kevin Kuszyk <kevin.kuszyk@ukhsa.gov.uk>
2023-09-18 15:44:48 +0100

    CDD-1270 Adding iam role and new object notification and queue for sentinel

---
+++ terraform/10-account/iam.sentinel-role.tf
@@ -0,0 +1,50 @@
+module "iam_sentinel_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.29.0"
+
+  create_role          = true
+  max_session_duration = 3600
+  role_name            = "uhd-sentinel-role"
+  role_requires_mfa    = false
+  role_sts_externalid  = "5767722a-3166-4a1e-97c3-2ec130dae7d7"
+
+  custom_role_policy_arns = [
+    module.iam_sentinel_policy.arn
+  ]
+
+  trusted_role_arns = [
+    "arn:aws:iam::197857026523:root",
+  ]
+}
+
+module "iam_sentinel_policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "5.30.0"
+
+  name = "uhd-sentinel-policy"
+
+  policy = jsonencode(
+    {
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Action = [
+            "SQS:ChangeMessageVisibility",
+            "SQS:DeleteMessage",
+            "SQS:ReceiveMessage",
+            "SQS:GetQueueUrl"
+          ],
+          Effect   = "Allow",
+          Resource = module.sqs_sentinel_alb_access_logs.queue_arn
+        },
+        {
+          Action = [
+            "s3:GetObject"
+          ],
+          Effect   = "Allow",
+          Resource = module.s3_elb_logs.s3_bucket_arn
+        }
+      ]
+    }
+  )
+}
