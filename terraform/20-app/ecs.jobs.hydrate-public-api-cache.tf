a8e39238ff00277f00b70d9072d907d2cc55356f
A-Ashiq <afaan.ashiq@nate.tech>
2023-09-27 16:15:35 +0100

    Rename `API_KEY` -> `CDN_AUTH_KEY`

--- terraform/20-app/ecs.jobs.hydrate-public-api-cache.tf
+++ terraform/20-app/ecs.jobs.hydrate-public-api-cache.tf
@@ -2,7 +2,7 @@ resource "local_sensitive_file" "ecs_job_hydrate_public_api_cache" {
   filename = "ecs-jobs/hydrate-public-api-cache.json"
   content  = templatefile("ecs-jobs/hydrate-public-api-cache.tftpl", {
     public_api_url    = local.urls.public_api
-    api_key           = jsonencode(aws_secretsmanager_secret_version.cdn_public_api_secure_header_value.secret_string)
+    cdn_auth_key      = jsonencode(aws_secretsmanager_secret_version.cdn_public_api_secure_header_value.secret_string)
     cluster_arn       = module.ecs.cluster_arn
     security_group_id = module.ecs_service_public_api.security_group_id
     subnet_ids        = module.vpc.private_subnets
