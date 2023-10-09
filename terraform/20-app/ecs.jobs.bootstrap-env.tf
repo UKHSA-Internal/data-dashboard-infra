017a83037accdd1482f233cc6ddd966413747db7
A-Ashiq <afaan.ashiq@nate.tech>
2023-10-09 14:43:58 +0100

    Remove api key from being passed into `bootstrap-env` ECS job

--- terraform/20-app/ecs.jobs.bootstrap-env.tf
+++ terraform/20-app/ecs.jobs.bootstrap-env.tf
@@ -2,7 +2,6 @@ resource "local_sensitive_file" "ecs_job_bootstrap_env" {
   filename = "ecs-jobs/bootstrap-env.json"
   content = templatefile("ecs-jobs/bootstrap-env.tftpl", {
     cms_admin_user_password = random_password.cms_admin_user_password.result
-    private_api_key         = local.private_api_key
     cluster_arn             = module.ecs.cluster_arn
     security_group_id       = module.ecs_service_ingestion.security_group_id
     subnet_ids              = module.vpc.private_subnets
