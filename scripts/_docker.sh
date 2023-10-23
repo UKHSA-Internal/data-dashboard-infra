3816fbdda8186ed388f8e81eab46542da9f704d9
Kevin Kuszyk <kevin.kuszyk@ukhsa.gov.uk>
2023-10-23 17:45:45 +0100

    CDD-1375 Adding `uhd docker build` command

--- scripts/_docker.sh
+++ scripts/_docker.sh
@@ -7,6 +7,8 @@ function _docker_help() {
     echo "commands:"
     echo "  help                 - this help screen"
     echo
+    echo "  build [repo]         - build a docker image for the specified repo"
+    echo
     echo "  pull                 - pull the latest source images from the dev account"
     echo "  push                 - push images to your dev ECR"
     echo "  push <account> <env> - tag and push images"
@@ -23,12 +25,36 @@ function _docker() {
     local args=(${@:2})
 
     case $verb in
+        "build") _docker_build $args ;;
         "pull") _docker_pull $args ;;
         "push") _docker_push $args ;;
         "ecr:login") _docker_ecr_login $args ;;
 
         *) _docker_help ;;
     esac
+}
+
+function _docker_build() {
+    local repo=$1
+
+    if [[ -z ${repo} ]]; then
+        echo "Repo is required" >&2
+        return 1
+    fi
+
+    local dev_account_id=$(_get_target_aws_account_id "dev")
+    local env=$(_get_env_name)
+
+    cd $root/../winter-pressures-$repo
+
+    local ecr_repo_name=$repo
+    [[ "$repo" == "frontend" ]] && ecr_repo_name=("front-end")
+
+    echo "Building docker image for $repo"
+
+    docker buildx build --platform linux/amd64 -t ${dev_account_id}.dkr.ecr.eu-west-2.amazonaws.com/uhd-${env}-${ecr_repo_name}:latest --push .
+    
+    cd $root
 }
 
 function _docker_pull() {
@@ -68,10 +94,6 @@ function _docker_push() {
     done
 
     echo $dest | xargs -P10 -n1 docker push
-}
-
-function _docker_ecr_login() {
-    _docker_ecr_login_account "dev"
 }
 
 function _docker_ecr_login() {
