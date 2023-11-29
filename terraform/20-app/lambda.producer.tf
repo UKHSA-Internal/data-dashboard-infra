4386f59db676c239d8b709aeb4d455a8770e35ed
A-Ashiq <afaan.ashiq@nate.tech>
2023-11-29 13:26:57 +0000

    Add lambda producer to read files as they arrive in s3 and write to kinesis

---
+++ terraform/20-app/lambda.producer.tf
@@ -0,0 +1,32 @@
+module "lambda_producer" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "6.4.0"
+  function_name = "${local.prefix}-producer"
+  description   = "Acts as the conduit between the S3 ingest bucket and the Kinesis data stream."
+
+  create_package = true
+  runtime        = "python3.11"
+  handler        = "producer.handler"
+  source_path    = "./lambda"
+
+  maximum_retry_attempts = 1
+  timeout                = 60         # Timeout after 1 minute
+
+  environment_variables = {
+    KINESIS_DATA_STREAM_NAME = aws_kinesis_stream.kinesis_data_stream_ingestion.name
+  }
+
+  attach_policy_statements = true
+  policy_statements        = {
+    get_items_from_in_folder_of_ingest_bucket = {
+      actions   = ["s3:GetObject"]
+      effect    = "Allow"
+      resources = ["${module.s3_ingest.s3_bucket_arn}/in/*"]
+    }
+    write_to_kinesis = {
+      effect    = "Allow"
+      actions   = ["kinesis:PutRecord"]
+      resources = [aws_kinesis_stream.kinesis_data_stream_ingestion.arn]
+    }
+  }
+}
