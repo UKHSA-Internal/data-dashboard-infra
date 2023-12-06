10db7a57fd5726c4075e5ed21309862dc49cc2b9
Kevin Kuszyk <kevin.kuszyk@ukhsa.gov.uk>
2023-12-06 14:40:10 +0000

    CDD-1409 Adding cloud front viewer request function to normalize accept header on public api

---
+++ src/public-api-cloud-front-viewer-request/index.test.js
@@ -0,0 +1,93 @@
+const originRequest = require("./index");
+const handler = originRequest.__get__("handler");
+
+test("Headers should not be removed", () => {
+  const event = {
+    request: {
+      uri: "https://foo.bar/baz",
+      headers: {
+        accept: { value: "text/html" },
+        "x-bar": { value: "baz" },
+        "x-foo": { value: "bar" },
+      },
+    },
+  };
+
+  const expected = {
+    accept: { value: "text/html" },
+    "x-bar": { value: "baz" },
+    "x-foo": { value: "bar" },
+  };
+
+  const result = handler(event).headers;
+
+  expect(result).toEqual(expected);
+});
+
+test.each([
+  { accept: "text/html, foo/bar", expected: "text/html" },
+  { accept: "application/json, foo/bar", expected: "application/json" },
+  {
+    accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    expected: "text/html",
+  },
+  {
+    accept: "application/json,text/*;q=0.99",
+    expected: "application/json",
+  },
+  {
+    accept: "",
+    expected: "application/json",
+  },
+])(
+  "Accept header '$accept' should normalized to '$expected'",
+  ({ accept, expected }) => {
+    const event = {
+      request: {
+        headers: {
+          accept: { value: accept },
+        },
+      },
+    };
+
+    const result = handler(event);
+
+    expect(result.headers.accept.value).toEqual(expected);
+  }
+);
+
+test.each([
+  "image/webp,image/avif,image/jxl,image/heic,image/heic-sequence,video/*;q=0.8,image/png,image/svg+xml,image/*;q=0.8,*/*;q=0.5",
+  "text/css,*/*;q=0.1",
+  "*/*",
+])(
+  "Accept headers for images, css etc should be normalized to '' - test for '%s'",
+  (accept) => {
+    const event = {
+      request: {
+        headers: {
+          accept: { value: accept },
+        },
+      },
+    };
+
+    const result = handler(event);
+
+    expect(result.headers.accept.value).toEqual("");
+  }
+);
+
+test("When accept header is missing it defaults to application/json", () => {
+  const event = {
+    request: {
+      headers: {
+        "x-bar": { value: "baz" },
+        "x-foo": { value: "bar" },
+      },
+    },
+  };
+
+  const result = handler(event);
+
+  expect(result.headers.accept.value).toEqual("application/json");
+});
