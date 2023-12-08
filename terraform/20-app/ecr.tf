d03b622a09d5abf80b5913c79182715ae9e00ad3
A-Ashiq <afaan.ashiq@nate.tech>
2023-12-08 08:52:11 +0000

    Add ECR for ingestion image

--- terraform/20-app/ecr.tf
+++ terraform/20-app/ecr.tf
@@ -20,6 +20,37 @@ module "ecr_api" {
   repository_read_write_access_arns = ["arn:aws:iam::${var.tools_account_id}:root"]
 }
 
+
+module "ecr_ingestion" {
+  source  = "terraform-aws-modules/ecr/aws"
+  version = "1.6.0"
+
+  repository_force_delete            = true
+  repository_image_tag_mutability    = "MUTABLE"
+  repository_name                    = "${local.prefix}-ingestion"
+  repository_read_access_arns        = ["arn:aws:iam::${var.assume_account_id}:root"]
+  repository_read_write_access_arns  = ["arn:aws:iam::${var.tools_account_id}:root"]
+
+  create_lifecycle_policy     = true
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 images",
+        selection    = {
+          tagStatus   = "any",
+          countType   = "imageCountMoreThan",
+          countNumber = 10
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+
 module "ecr_front_end" {
   source  = "terraform-aws-modules/ecr/aws"
   version = "1.6.0"
