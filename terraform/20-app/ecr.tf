489b3531be735e886c68183e30905241825859f2
A-Ashiq <afaan.ashiq@nate.tech>
2023-12-11 14:59:51 +0000

    Add image provisioner to populate ECR with image so the lambda function can pull an initial image

--- terraform/20-app/ecr.tf
+++ terraform/20-app/ecr.tf
@@ -8,6 +8,19 @@ moved {
   to   = module.ecr_front_end.aws_ecr_repository.this[0]
 }
 
+module "ecr_front_end" {
+  source  = "terraform-aws-modules/ecr/aws"
+  version = "1.6.0"
+
+  create_lifecycle_policy           = false
+  repository_force_delete           = true
+  repository_image_tag_mutability   = "MUTABLE"
+  repository_name                   = "${local.prefix}-front-end"
+  repository_read_access_arns       = ["arn:aws:iam::${var.assume_account_id}:root"]
+  repository_read_write_access_arns = ["arn:aws:iam::${var.tools_account_id}:root"]
+
+}
+
 module "ecr_api" {
   source  = "terraform-aws-modules/ecr/aws"
   version = "1.6.0"
@@ -20,6 +33,24 @@ module "ecr_api" {
   repository_read_write_access_arns = ["arn:aws:iam::${var.tools_account_id}:root"]
 }
 
+
+# This is a 1-time execution script to put an image into the ingestion ECR repo
+# So that when the ingestion lambda function is provisioned by terraform,
+# it will have an image to pull from.
+# Otherwise there is a chicken-egg scenario whereby the lambda can't be provisioned
+# because there would be no image in the ECR
+resource "terraform_data" "image_provisioner" {
+  depends_on = [module.ecr_ingestion]
+  provisioner "local-exec" {
+    command = <<EOF
+      source ../../uhd.sh
+      uhd docker ecr:login
+      uhd docker pull
+      uhd docker ecr:login ${var.environment_type}
+      uhd docker push ${var.environment_type} ${local.environment}
+    EOF
+  }
+}
 
 module "ecr_ingestion" {
   source  = "terraform-aws-modules/ecr/aws"
@@ -49,18 +80,4 @@ module "ecr_ingestion" {
       }
     ]
   })
-}
-
-
-module "ecr_front_end" {
-  source  = "terraform-aws-modules/ecr/aws"
-  version = "1.6.0"
-
-  create_lifecycle_policy           = false
-  repository_force_delete           = true
-  repository_image_tag_mutability   = "MUTABLE"
-  repository_name                   = "${local.prefix}-front-end"
-  repository_read_access_arns       = ["arn:aws:iam::${var.assume_account_id}:root"]
-  repository_read_write_access_arns = ["arn:aws:iam::${var.tools_account_id}:root"]
-
 }
