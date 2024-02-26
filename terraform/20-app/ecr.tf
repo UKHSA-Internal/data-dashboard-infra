d62b00c5123eca01f55a2ca16fc3b2b8044f0813
A-Ashiq <afaan.ashiq@nate.tech>
2024-02-26 17:36:23 +0000

    Keep latest 30 images as standard policy across ECRs

--- terraform/20-app/ecr.tf
+++ terraform/20-app/ecr.tf
@@ -13,11 +13,10 @@ locals {
     rules = [
       {
         rulePriority = 1,
-        description  = "Expire images older than 30 days",
+        description  = "Keep the last 30 images",
         selection    = {
           tagStatus   = "any",
-          countType   = "sinceImagePushed",
-          countUnit   = "days"
+          countType   = "imageCountMoreThan",
           countNumber = 30
         },
         action = {
