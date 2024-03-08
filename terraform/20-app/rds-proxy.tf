a4b1ae03d3e4ac50c799146c74f610cfaee2fc83
A-Ashiq <afaan.ashiq@nate.tech>
2024-03-08 09:29:10 +0000

    Add security group for RDS proxy allowing traffic in from ingestion lambda and out to db

--- terraform/20-app/rds-proxy.tf
+++ terraform/20-app/rds-proxy.tf
@@ -2,16 +2,8 @@ module "rds_proxy" {
   source = "terraform-aws-modules/rds-proxy/aws"
 
   name                   = "${local.prefix}-rds-proxy"
-  vpc_subnet_ids         = module.vpc.private_subnets
-  vpc_security_group_ids = [module.app_rds_security_group.security_group_id]
-
-  endpoints = {
-    read_write = {
-      name                   = "read-write-endpoint"
-      vpc_subnet_ids         = module.vpc.private_subnets
-      vpc_security_group_ids = [module.app_rds_security_group.security_group_id]
-    }
-  }
+  vpc_subnet_ids         = module.vpc.database_subnets
+  vpc_security_group_ids = [module.rds_proxy_security_group.security_group_id]
 
   auth = {
     api_user = {
@@ -23,4 +15,29 @@ module "rds_proxy" {
   debug_logging          = true
   target_db_instance     = true
   db_instance_identifier = aws_db_instance.app_rds_primary.identifier
+}
+
+
+module "rds_proxy_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+
+  name   = "${local.prefix}-app-db"
+  vpc_id = module.vpc.vpc_id
+
+  ingress_with_source_security_group_id = [
+    {
+      description              = "ingestion lambda to proxy"
+      rule                     = "postgresql-tcp"
+      source_security_group_id = module.lambda_ingestion_security_group.security_group_id
+    },
+  ]
+
+  egress_with_source_security_group_id = [
+    {
+      description              = "proxy to db"
+      rule                     = "postgresql-tcp"
+      source_security_group_id = module.app_rds_security_group.security_group_id
+    },
+  ]
 }
