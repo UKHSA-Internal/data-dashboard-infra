57508a6360e9126f13e9ad627f7ca3084a9e974c
A-Ashiq <afaan.ashiq@nate.tech>
2024-03-08 09:29:49 +0000

    Allow traffic into db from RDS proxy

--- terraform/20-app/rds.tf
+++ terraform/20-app/rds.tf
@@ -123,14 +123,14 @@ module "app_rds_security_group" {
       source_security_group_id = module.ecs_service_cms_admin.security_group_id
     },
     {
-      description              = "ingestion lambda to db"
-      rule                     = "postgresql-tcp"
-      source_security_group_id = module.lambda_ingestion_security_group.security_group_id
-    },
-    {
       description              = "utility worker tasks to db"
       rule                     = "postgresql-tcp"
       source_security_group_id = module.ecs_service_utility_worker.security_group_id
+    },
+    {
+      description              = "proxy to db"
+      rule                     = "postgresql-tcp"
+      source_security_group_id = module.rds_proxy_security_group.security_group_id
     },
   ]
 }
