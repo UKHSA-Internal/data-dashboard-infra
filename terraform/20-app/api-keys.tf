4ae1d3d564bd4e27ba374a8f5ca3bf9a64a3b3e8
A-Ashiq <afaan.ashiq@nate.tech>
2024-03-15 16:37:48 +0000

    Generate API key for clients of feature flag service

--- terraform/20-app/api-keys.tf
+++ terraform/20-app/api-keys.tf
@@ -23,6 +23,24 @@ resource "random_password" "backend_cryptographic_signing_key" {
   special     = true
 }
 
+resource "random_password" "feature_flags_client_api_key" {
+  length      = 56
+  min_numeric = 1
+  min_lower   = 1
+  special     = false
+  upper       = false
+}
+
+resource "random_password" "feature_flags_admin_user_password" {
+  length      = 20
+  min_numeric = 1
+  min_lower   = 1
+  min_upper   = 1
+  min_special = 1
+  special     = true
+}
+
 locals {
-  private_api_key = "${random_password.private_api_key_prefix.result}.${random_password.private_api_key_suffix.result}"
+  feature_flags_client_api_key = "*:production.${random_password.feature_flags_client_api_key.result}"
+  private_api_key              = "${random_password.private_api_key_prefix.result}.${random_password.private_api_key_suffix.result}"
 }
