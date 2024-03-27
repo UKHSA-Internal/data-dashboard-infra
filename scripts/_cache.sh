559e0b3fed398e384751c9236416b7ff44574995
A-Ashiq <afaan.ashiq@nate.tech>
2024-03-27 19:16:56 +0000

    Supress exit code 255 timeout when flushing caches

--- scripts/_cache.sh
+++ scripts/_cache.sh
@@ -50,6 +50,8 @@ function _cache_flush() {
 
     echo "Filling the public api cloud front cache..." 
     uhd cache fill-public-api --wait
+
+    _supress_cache_flush_timeout_exit_code
 }
 
 function _cache_flush_redis() {
@@ -155,4 +157,12 @@ function _get_distribution_id() {
     local distribution_id=$(jq -r --arg distribution "$distribution" '.cloud_front.value[$distribution]' $terraform_output_file)
 
     echo $distribution_id
+}
+
+function _supress_cache_flush_timeout_exit_code() {
+    local exit_code=$?
+    if [[ $exit_code = 255 ]]; then
+      echo "Not waiting for cache fill to complete..."
+      return 0
+    fi
 }
