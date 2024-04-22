281eb1620c414b4c9d7ec4fc0615d89a135b5736
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-22 16:42:47 +0100

    Add serverless aurora db cluster

---
+++ terraform/20-app/aurora-db.main.tf
@@ -0,0 +1,28 @@
+module "aurora_db_main" {
+  source = "terraform-aws-modules/rds-aurora/aws"
+
+  name              = "${local.prefix}-aurora-db-main"
+  engine            = "aurora-postgresql"
+  engine_mode       = "provisioned"
+  engine_version    = "15.5"
+  storage_encrypted = true
+
+  replication_source_identifier = aws_db_instance.app_rds_primary.arn
+  manage_master_user_password   = false
+
+  monitoring_interval = 0
+  apply_immediately   = true
+  skip_final_snapshot = true
+
+  instance_class = "db.serverless"
+  serverlessv2_scaling_configuration = {
+    min_capacity = 1
+    max_capacity = 10
+  }
+  instances = {
+    1 = {}
+  }
+
+  vpc_id               = module.vpc.vpc_id
+  db_subnet_group_name = module.vpc.database_subnet_group_name
+}
