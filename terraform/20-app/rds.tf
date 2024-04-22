68c7b1fde251dfa304389ac7cf60bfc0e211872f
A-Ashiq <afaan.ashiq@nate.tech>
2024-04-22 15:26:07 +0100

    Switch ingestion lambda to point directly at RDS primary

--- terraform/20-app/rds.tf
+++ terraform/20-app/rds.tf
@@ -128,9 +128,9 @@ module "app_rds_security_group" {
       source_security_group_id = module.ecs_service_utility_worker.security_group_id
     },
     {
-      description              = "rds proxy to db"
+      description              = "ingestion lambda to db"
       rule                     = "postgresql-tcp"
-      source_security_group_id = module.rds_proxy_security_group.security_group_id
+      source_security_group_id = module.lambda_ingestion_security_group.security_group_id
     },
   ]
 }
