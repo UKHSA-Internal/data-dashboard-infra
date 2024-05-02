04cadf98b3aaeeff2820ae541f3add19e69c3609
A-Ashiq <afaan.ashiq@nate.tech>
2024-05-02 09:52:20 +0100

    Add serverless v2 aurora db cluster

--- terraform/20-app/aurora-db.app.tf
+++ terraform/20-app/aurora-db.app.tf
@@ -1,20 +1,23 @@
-module "aurora_db_main" {
+module "aurora_db_app" {
   source = "terraform-aws-modules/rds-aurora/aws"
 
-  name              = "${local.prefix}-aurora-db-main"
-  engine            = "aurora-postgresql"
-  engine_mode       = "provisioned"
-  engine_version    = "15.5"
-  storage_encrypted = true
-  kms_key_id        = module.kms_app_rds.key_arn
+  name                    = "${local.prefix}-aurora-db-app"
+  engine                  = "aurora-postgresql"
+  engine_mode             = "provisioned"
+  engine_version          = "15.5"
+  storage_encrypted       = true
+  backup_retention_period = 35
+  kms_key_id              = module.kms_app_rds.key_arn
 
   is_primary_cluster            = false
   replication_source_identifier = aws_db_instance.app_rds_primary.arn
   manage_master_user_password   = false
+  database_name                 = "cms"
 
   monitoring_interval = 0
   apply_immediately   = true
   skip_final_snapshot = true
+  publicly_accessible = local.enable_public_db
 
   instance_class = "db.serverless"
   serverlessv2_scaling_configuration = {
