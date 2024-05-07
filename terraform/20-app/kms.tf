e96b0ed952a21d0d89c411f3bd970faf726eabf0
A-Ashiq <afaan.ashiq@nate.tech>
2024-05-07 16:09:03 +0100

    Reference aurora in place of deprecated RDS

--- terraform/20-app/kms.tf
+++ terraform/20-app/kms.tf
@@ -1,14 +1,14 @@
-module "kms_app_rds" {
+module "kms_app_aurora" {
 	source  = "terraform-aws-modules/kms/aws"
 	version = "2.2.1"
 
-	description             = "RDS encryption key"
+	description             = "Aurora encryption key"
 	key_usage               = "ENCRYPT_DECRYPT"
 	deletion_window_in_days = 7
     multi_region            = false
 	
 	key_owners          	= ["arn:aws:iam::${var.tools_account_id}:root"]
 	
-    aliases                 = ["${local.prefix}-app-rds"]
+    aliases                 = ["${local.prefix}-app-aurora"]
 	aliases_use_name_prefix = true
 }
