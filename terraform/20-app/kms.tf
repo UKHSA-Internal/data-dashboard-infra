babe6b3ffe42df88ff8b570434e6f876c8414eff
A-Ashiq <afaan.ashiq@nate.tech>
2024-05-07 16:15:45 +0100

    Rename KMS alias

--- terraform/20-app/kms.tf
+++ terraform/20-app/kms.tf
@@ -1,14 +1,14 @@
 module "kms_app_aurora" {
-	source  = "terraform-aws-modules/kms/aws"
-	version = "2.2.1"
+  source  = "terraform-aws-modules/kms/aws"
+  version = "2.2.1"
 
-	description             = "Aurora encryption key"
-	key_usage               = "ENCRYPT_DECRYPT"
-	deletion_window_in_days = 7
-    multi_region            = false
-	
-	key_owners          	= ["arn:aws:iam::${var.tools_account_id}:root"]
-	
-    aliases                 = ["${local.prefix}-app-aurora"]
-	aliases_use_name_prefix = true
+  description             = "Aurora encryption key"
+  key_usage               = "ENCRYPT_DECRYPT"
+  deletion_window_in_days = 7
+  multi_region            = false
+
+  key_owners = ["arn:aws:iam::${var.tools_account_id}:root"]
+
+  aliases                 = ["${local.prefix}-app-rds"]
+  aliases_use_name_prefix = true
 }
