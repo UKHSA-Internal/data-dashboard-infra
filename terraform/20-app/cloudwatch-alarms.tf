c0a926fa24079cba39292f3fae36b6a7884b4fe5
A-Ashiq <afaan.ashiq@nate.tech>
2024-05-30 11:07:52 +0100

    Add cloudwatch alarm for aurora db for CPU utilization

--- terraform/20-app/cloudwatch-alarms.tf
+++ terraform/20-app/cloudwatch-alarms.tf
@@ -37,3 +37,23 @@ module "cloudwatch_alarm_cloudfront_frontend_400_errors" {
   metric_name = "4xxErrorRate"
   statistic   = "Average"
 }
+
+module "cloudwatch_alarm_aurora_db_app" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
+  version = "5.3.1"
+
+  alarm_name          = "${local.prefix}-aurora-db-app"
+  alarm_description   = "CPU utilization of aurora db application cluster"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  threshold           = 90
+  period              = 60
+  unit                = "None"
+  dimensions = {
+    DBClusterIdentifier = module.aurora_db_app.cluster_id
+    SourceRegion        = local.region
+  }
+  namespace   = "AWS/RDS"
+  metric_name = "CPUUtilization"
+  statistic   = "Average"
+}
