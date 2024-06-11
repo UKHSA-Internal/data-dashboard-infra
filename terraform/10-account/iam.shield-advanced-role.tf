015a3d55a06afa897738bdd3052d3f7ad842519f
A-Ashiq <afaan.ashiq@nate.tech>
2024-06-11 17:07:48 +0100

    Set `"drt.shield.amazonaws.com"` as trusted role service on IAM role

--- terraform/10-account/iam.shield-advanced-role.tf
+++ terraform/10-account/iam.shield-advanced-role.tf
@@ -7,30 +7,6 @@ module "shield_advanced_drt_role" {
   role_name            = "ShieldAdvancedDRTRole"
   role_requires_mfa    = false
 
-  custom_role_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AWSShieldDRTAccessPolicy",
-    module.iam_shield_advanced_drt_policy.arn,
-  ]
-}
-
-module "iam_shield_advanced_drt_policy" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-  version = "5.37.1"
-
-  name = "uhd-shield-advanced-drt-policy"
-
-  policy = jsonencode(
-    {
-      Version   = "2012-10-17",
-      Statement = [
-        {
-          "Effect" : "Allow",
-          "Principal" : {
-            "Service" : "drt.shield.amazonaws.com"
-          },
-          "Action" : "sts:AssumeRole"
-        },
-      ]
-    }
-  )
+  custom_role_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSShieldDRTAccessPolicy"]
+  trusted_role_services   = ["drt.shield.amazonaws.com"]
 }
