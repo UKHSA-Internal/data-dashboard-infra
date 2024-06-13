ebde015fd03a2b8e38ff30ca2e990f58e528b051
A-Ashiq <afaan.ashiq@nate.tech>
2024-06-13 09:17:08 +0100

    Add Tom H 2nd IP address to allow list

--- terraform/20-app/ip-allow-lists.tf
+++ terraform/20-app/ip-allow-lists.tf
@@ -35,6 +35,7 @@ locals {
       "86.128.102.66/32",   # Ester
       "172.29.176.6/32",    # Splunk synthetic monitioring - Azure UK South
       "167.98.243.140/32",  # Tom H
+      "81.105.235.133/32",  # Tom H 2
       "51.149.2.8/32",      # Agostinho Sousa
       "86.29.186.201/32",   # Charlotte Brace
       "2.221.74.175/32",    # Gareth
