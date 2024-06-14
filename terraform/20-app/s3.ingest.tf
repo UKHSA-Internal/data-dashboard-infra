639119fbce9898eb4aeb13c0a6c62c62188ad0e3
A-Ashiq <afaan.ashiq@nate.tech>
2024-06-14 16:16:47 +0100

    Capture logs from `ingest` and `archive-web-content` buckets and send them to central bucket of s3 access logs

--- terraform/20-app/s3.ingest.tf
+++ terraform/20-app/s3.ingest.tf
@@ -1,11 +1,20 @@
+locals {
+  s3_ingest_bucket_name = "${local.prefix}-ingest"
+}
+
 module "s3_ingest" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.1.2"
 
-  bucket = "${local.prefix}-ingest"
+  bucket = local.s3_ingest_bucket_name
 
   attach_deny_insecure_transport_policy = true
   force_destroy                         = true
+
+  logging = {
+    target_bucket = data.aws_s3_bucket.s3_access_logs.id
+    target_prefix = "${local.s3_ingest_bucket_name}/"
+  }
 
   lifecycle_rule = [
     {
