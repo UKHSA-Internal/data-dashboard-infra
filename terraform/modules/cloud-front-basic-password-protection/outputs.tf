76947003ce91b131e9b000d3f9500c07ef3e8e39
A-Ashiq <afaan.ashiq@nate.tech>
2024-06-21 11:56:50 +0100

    Return ARN from `aws_cloudfront_function` resource if `create` var provided as true

--- terraform/modules/cloud-front-basic-password-protection/outputs.tf
+++ terraform/modules/cloud-front-basic-password-protection/outputs.tf
@@ -1,4 +1,4 @@
 output "arn" {
   description = "The ARN of the Cloudfront function"
-  value = aws_cloudfront_function.password_protection[0].arn
+  value       = var.create ? aws_cloudfront_function.password_protection[0].arn : ""
 }
