48c69cc73954d6292025afcb3509e622618a67fc
A-Ashiq <afaan.ashiq@nate.tech>
2024-06-21 10:55:46 +0100

    Add KV store and associated Cloudfront function to be used to add basic password protection in front of select CF distros

---
+++ terraform/20-app/cloud-front.password-protection.tf
@@ -0,0 +1,60 @@
+resource "aws_cloudfront_key_value_store" "password_protection" {
+  name = "${local.prefix}-password-protection"
+}
+
+resource "aws_cloudfront_function" "password_protection" {
+  name                         = "${local.prefix}-password-protection"
+  runtime                      = "cloudfront-js-2.0"
+  publish                      = true
+  key_value_store_associations = [
+    aws_cloudfront_key_value_store.password_protection.arn
+  ]
+  code = <<-EOF
+  const cf = require('cloudfront')
+
+  async function getCredentials(KvStoreId) {
+      const kvsHandle = cf.kvs(KvStoreId);
+      const username = await kvsHandle.get("username");
+      const password = await kvsHandle.get("password");
+      return {
+          username: username,
+          password: password,
+      }
+  }
+
+  function extractFinalSection(inputString) {
+      const parts = inputString.split('/');
+      return parts[parts.length - 1];
+  }
+
+  function buildEncodedString(credentials) {
+      const encodedCredentials = btoa(`$${credentials.username}:$${credentials.password}`);
+      return `Basic $${encodedCredentials}`
+  }
+
+  async function handler(event) {
+      const authorizationHeaders = event.request.headers.authorization;
+      const kvStoreArn = "${aws_cloudfront_key_value_store.password_protection.arn}"
+      console.log('arn')
+      console.log(kvStoreArn)
+      const KvStoreId = extractFinalSection(kvStoreArn)
+
+      const credentials = await getCredentials(KvStoreId)
+      const encodedString = buildEncodedString(credentials)
+
+      if (authorizationHeaders && authorizationHeaders.value === encodedString) {
+          return event.request;
+      }
+
+      return {
+          statusCode: 401,
+          statusDescription: "Unauthorized",
+          headers: {
+              "www-authenticate": {value: 'Basic'},
+          },
+      };
+  }
+
+  handler;
+  EOF
+}
