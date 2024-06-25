d9a7660619f658daa3a55160b608aa298f8846c5
A-Ashiq <afaan.ashiq@nate.tech>
2024-06-25 13:25:36 +0100

    Move Cloudfront cloudwatch alarms & SNS topic to us-east-1

--- terraform/20-app/cloudwatch-alarms.tf
+++ terraform/20-app/cloudwatch-alarms.tf
@@ -1,10 +1,9 @@
-module "cloudwatch_alarm_cloudfront_frontend_500_errors" {
-  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
-  version = "5.3.1"
+resource "aws_cloudwatch_metric_alarm" cloudfront_frontend_500_errors {
+  provider = aws.us_east_1
 
   alarm_name          = "${local.prefix}-cloudfront-frontend-5xx-alarm"
   alarm_description   = "HTTP 5xx errors in the frontend Cloudfront distribution."
-  alarm_actions       = [module.sns_topic_alarms.topic_arn]
+  alarm_actions       = [aws_sns_topic.sns_topic_alarms.arn]
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
   threshold           = 1
@@ -19,13 +18,12 @@ module "cloudwatch_alarm_cloudfront_frontend_500_errors" {
   statistic   = "Average"
 }
 
-module "cloudwatch_alarm_cloudfront_frontend_400_errors" {
-  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
-  version = "5.3.1"
+resource "aws_cloudwatch_metric_alarm" cloudfront_frontend_400_errors {
+  provider = aws.us_east_1
 
   alarm_name          = "${local.prefix}-cloudfront-frontend-4xx-alarm"
   alarm_description   = "HTTP 4xx errors in the frontend Cloudfront distribution."
-  alarm_actions       = [module.sns_topic_alarms.topic_arn]
+  alarm_actions       = [aws_sns_topic.sns_topic_alarms.arn]
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
   threshold           = 1
@@ -46,7 +44,7 @@ module "cloudwatch_alarm_aurora_db_app" {
 
   alarm_name          = "${local.prefix}-aurora-db-app"
   alarm_description   = "CPU utilization of aurora db application cluster"
-  alarm_actions       = [module.sns_topic_alarms.topic_arn]
+  alarm_actions       = [aws_sns_topic.sns_topic_alarms.arn]
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
   threshold           = 90
--- terraform/20-app/sns.tf
+++ terraform/20-app/sns.tf
@@ -1,11 +1,11 @@
-module "sns_topic_alarms" {
-  source = "terraform-aws-modules/sns/aws"
+resource "aws_sns_topic" "sns_topic_alarms" {
   name   = "${local.prefix}-alarms"
+  provider = aws.us_east_1
+}
 
-  subscriptions = {
-    lambda = {
-      protocol = "lambda"
-      endpoint = module.lambda_alarm_notification.lambda_function_arn
-    }
-  }
+resource "aws_sns_topic_subscription" "this" {
+  provider = aws.us_east_1
+  endpoint = module.lambda_alarm_notification.lambda_function_arn
+  protocol = "lambda"
+  topic_arn = aws_sns_topic.sns_topic_alarms.arn
 }
