99b2dd245e479ddc364e12101030f5543ed5dbf3
A-Ashiq <afaan.ashiq@nate.tech>
2024-07-26 18:15:02 +0100

    Add `cloud-watch-canary` module

---
+++ terraform/modules/cloud-watch-canary/vars.tf
@@ -0,0 +1,40 @@
+variable "create" {
+  description = "Whether to create the synthetic canary and its associated components"
+  type        = bool
+  default     = false
+}
+
+variable "name" {
+  description = "The name to associate with the synthetic canary components"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC to deploy the canary within"
+  type        = string
+}
+
+variable "s3_access_logs_id" {
+  description = "The ID of the S3 bucket where access logs are sent to"
+  type        = string
+}
+
+variable "s3_logs_destination" {
+  description = "Map containing the ID and the ARN of the S3 bucket where the results of the canary are to be sent."
+  type        = map(string)
+}
+
+variable "subnet_ids" {
+  description = "The IDs of the subnets where this canary is to run."
+  type        = list(string)
+}
+
+variable "schedule_expression" {
+  description = "The cron schedule expression to apply to the canary."
+  type        = string
+}
+
+variable "script_path" {
+  description = "The file path of the script to attach to the canary"
+  type        = string
+}
