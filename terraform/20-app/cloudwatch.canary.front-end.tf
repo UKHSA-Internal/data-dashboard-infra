252eb68c32098dd844716bf9452dd971b24444f2
A-Ashiq <afaan.ashiq@nate.tech>
2024-07-30 14:56:45 +0100

    Implement canary to traverse frontend pages c/w screenshots

---
+++ terraform/20-app/cloudwatch.canary.front-end.tf
@@ -0,0 +1,21 @@
+module "cloudwatch_canary_front_end_screenshots" {
+  source            = "../modules/cloud-watch-canary"
+  create            = true
+  name              = "${local.prefix}-display"
+  s3_access_logs_id = data.aws_s3_bucket.s3_access_logs.id
+  s3_logs_destination = {
+    bucket_id  = module.s3_canary_logs.s3_bucket_id
+    bucket_arn = module.s3_canary_logs.s3_bucket_arn
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  schedule_expression = "rate(10 minutes)"
+  timeout_in_seconds  = 600
+  script_path         = "../../src/canary-front-end-screenshots"
+
+  environment_variables = {
+    SITEMAP_URL = "${local.urls.front_end}/sitemap.xml"
+  }
+}
