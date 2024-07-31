ffbbf13574f1a96ed271b99d1af41473351f99b4
A-Ashiq <afaan.ashiq@nate.tech>
2024-07-31 10:17:13 +0100

    Update output path

--- terraform/modules/cloud-watch-canary/script.tf
+++ terraform/modules/cloud-watch-canary/script.tf
@@ -1,12 +1,11 @@
 locals {
   script_content      = file("${var.script_path}/index.js")
   script_content_hash = sha256(local.script_content)
-  zip                 = "builds/${var.script_path}-${local.script_content_hash}.zip"
 }
 
 data "archive_file" "canary_script" {
   type        = "zip"
-  output_path = local.zip
+  output_path = "builds/${var.name}-${local.script_content_hash}.zip"
   source {
     content  = local.script_content
     filename = "nodejs/node_modules/index.js"
