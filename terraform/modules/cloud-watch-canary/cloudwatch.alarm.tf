f0b024e080e3ef8b11ff2b1885dc120fe6292392
A-Ashiq <afaan.ashiq@nate.tech>
2024-08-05 14:29:58 +0100

    Add corresponding Cloudwatch alarm to be raised when canary run fails

---
+++ terraform/modules/cloud-watch-canary/cloudwatch.alarm.tf
@@ -0,0 +1,23 @@
+module "cloudwatch_alarm" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
+  version = "5.3.1"
+
+  alarm_name          = "${var.name}-failed-alarm"
+  alarm_description   = jsonencode(
+    {
+      "description": "Alarm for SNS",
+      "s3_prefix": "canary/eu-west-2/${var.name}"
+    }
+  )
+  alarm_actions       = [module.sns_topic_alarm.topic_arn]
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  threshold           = 1
+  period              = 60
+  dimensions = {
+    CanaryName = aws_synthetics_canary.this.name
+  }
+  namespace   = "CloudWatchSynthetics"
+  metric_name = "Failed requests"
+  statistic   = "Sum"
+}
