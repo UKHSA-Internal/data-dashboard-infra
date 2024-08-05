213944c8891fdb8d9039704399b1cf1e34029b0e
A-Ashiq <afaan.ashiq@nate.tech>
2024-08-05 14:45:32 +0100

    Add source code for Lambda function used to send notifications after canary run failure

---
+++ src/lambda-canary-notification/index.js
@@ -0,0 +1,416 @@
+const {
+    S3Client,
+    GetObjectCommand,
+    ListObjectsV2Command,
+    ListObjectsV2CommandOutput,
+    GetObjectCommandOutput
+} = require("@aws-sdk/client-s3");
+const {SecretsManagerClient, GetSecretValueCommand, GetSecretValueCommandOutput} = require("@aws-sdk/client-secrets-manager");
+const {WebClient} = require("@slack/web-api")
+const axios = require('axios');
+const FormData = require('form-data');
+const path = require('path');
+
+const S3_CANARY_LOGS_BUCKET_NAME = process.env.S3_CANARY_LOGS_BUCKET_NAME;
+
+/**
+ * Gets the filename associated with the s3 key / file path
+ *
+ * @param {string} filePath - The full filepath / s3 key for the files
+ * @returns {string} - The extracted filename
+ */
+function getFilename(filePath) {
+    return path.basename(filePath);
+}
+
+/**
+ * Gets the secret for the Slack webhook URL from SecretsManager
+ *
+ * @param {SecretsManagerClient} secretsManagerClient - An instance of the SecretsManagerClient
+ *      to use for sending the command.
+ * @returns {object} - The response from secrets manager
+ */
+async function getSecret(secretsManagerClient) {
+    const input = {
+        "SecretId": process.env.SECRETS_MANAGER_SLACK_WEBHOOK_URL_ARN
+    };
+    const command = new GetSecretValueCommand(input);
+    return secretsManagerClient.send(command);
+}
+
+/**
+ * Gets and parses the secret for the Slack webhook URL from SecretsManager
+ *
+ * @param {SecretsManagerClient} secretsManagerClient - An optional instance of the SecretsManagerClient
+ *      to use for sending the command.
+ * @returns {object} - The JSON object representing the secret
+ */
+async function getSlackSecret(secretsManagerClient = new SecretsManagerClient()) {
+    const response = await getSecret(secretsManagerClient)
+    return JSON.parse(response.SecretString)
+}
+
+/**
+ * Lists all the files in the s3 bucket which contain the given prefix
+ *
+ * @param {string} bucket - The name of the S3 bucket.
+ * @param {string} prefix - The prefix to filter objects by in the S3 bucket.
+ * @param {S3Client} s3Client - An optional instance of the S3Client to use for sending the command.
+ * @returns {ListObjectsV2CommandOutput} - The response from the underlying `ListObjectsV2Command` call.
+ */
+async function listFiles(bucket, prefix, s3Client = new S3Client()) {
+    const command = new ListObjectsV2Command({
+        Bucket: bucket, Prefix: prefix
+    });
+    return s3Client.send(command);
+}
+
+/**
+ * Creates a file `Buffer` from the given `stream`
+ *
+ * @param {object} stream - The filestream to be opened.
+ * @returns {Promise} - The Promise object wrapping the created file buffer.
+ */
+const streamToBuffer = async (stream) => {
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+        stream.on('data', (chunk) => chunks.push(chunk));
+        stream.on('end', () => resolve(Buffer.concat(chunks)));
+        stream.on('error', reject);
+    });
+};
+
+/**
+ * Downloads the given file from the s3 bucket
+ *
+ * @param {string} bucket - The name of the S3 bucket.
+ * @param {string} key - The key associated with the target file being downloaded from the S3 bucket.
+ * @param {S3Client} s3Client - An optional instance of the S3Client to use for sending the command.
+ * @returns {GetObjectCommandOutput} - The response from the underlying `GetObjectCommand` call.
+ */
+async function downloadFile(bucket, key, s3Client = new S3Client()) {
+    const command = new GetObjectCommand({
+        Bucket: bucket, Key: key
+    });
+    return s3Client.send(command);
+}
+
+/**
+ * Downloads the given file from the s3 bucket
+ *
+ * @param {string} bucket - The name of the S3 bucket.
+ * @param {array} keys - The keys associated with the target files being downloaded from the S3 bucket.
+ * @param {S3Client} s3Client - An optional instance of the S3Client to use for sending the command.
+ * @returns {array} - A list of objects containing
+ *  {key: <the downloadable key>: content: The individual download responses}
+ */
+async function downloadAllFiles(keys, bucket, s3Client = new S3Client()) {
+    const results = [];
+
+    for (let key of keys) {
+        let content = await downloadFile(bucket, key, s3Client);
+        results.push({"key": key, "content": content});
+    }
+    return results;
+}
+
+/**
+ * Sets up the `WebClient` object used to interact with Slack.
+ *
+ * @param {string} slack_token - The token associated with the Slack bot.
+ * @returns {WebClient} - An instantiated `WebClient` instance used to interact with Slack.
+ */
+function buildSlackClient(slack_token) {
+    return new WebClient(slack_token);
+}
+
+/**
+ * Gets a URL from the Slack API to upload the file to.
+ *
+ * @param {WebClient} slackClient - The `WebClient` instance used to interact with Slack.
+ * @param {string} filename - The name of the file being uploaded to Slack.
+ * @param {number} length - Size in bytes of the file being uploaded.
+ * @returns {Object} - The response from the underlying `WebClient.files.getUploadURLExternal` call.
+ */
+async function getFileUploadURLToSlack(slackClient, filename, length) {
+    return slackClient.files.getUploadURLExternal({
+        token: slackClient.token, filename: filename, length: length
+    });
+};
+
+/**
+ * Sends a POST request to the given url containing the given `fileBufferStream`.
+ *
+ * @param {string} url - The URL to POST the file to.
+ * @param {string} filename - The name of the file being uploaded.
+ * @param {Buffer} fileBufferStream - Size in bytes of the file being uploaded.
+ * @returns {Promise} - The promise from the underlying POST request call
+ */
+async function sendPostRequest(url, filename, fileBufferStream) {
+    try {
+        const form = new FormData();
+        form.append('file', fileBufferStream, {filename});
+        await axios.post(url, form, {
+            headers: {...form.getHeaders()}
+        });
+    } catch (error) {
+        console.error('Error sending POST request:', error.message);
+    }
+}
+
+/**
+ * Finishes an upload to Slack started with a `WebClient.files.getUploadURLExternal` call
+ *
+ * @param {WebClient} slackClient - The `WebClient` instance used to interact with Slack.
+ * @param {array} files - Array of file ids and their corresponding (optional) titles.
+ * @param {string} channelId - The ID associated with the channel which the image is being sent to.
+ * @param {string} threadTs - The ID of the parent thread to attach this image as a reply to.
+ * @returns {Object} - The response from the underlying `WebClient.files.completeUploadExternal` call.
+ */
+async function completeFileUploadToSlack(slackClient, files, channelId, threadTs) {
+    return slackClient.files.completeUploadExternal({
+        token: slackClient.token, files: files, channel_id: channelId, thread_ts: threadTs,
+    });
+}
+
+/**
+ * Extracts the directory from the given full s3 object key
+ *
+ * @param {string} filePath - The `s3 object key / filepath to extract the directory from
+ * @returns {string} - The preceding file directory associated with the given full s3 object key.
+ */
+function getDirectoryPath(filePath) {
+    const parts = filePath.split('/');
+    parts.pop();
+    return parts.join('/');
+}
+
+/**
+ * Gets the current datetime containing the year, month, day and hour. The month, day and hours are padded to 2 digits.
+ *
+ * @returns {object} - The year, month, day, hour values returned as padded strings.
+ *   e.g. The date of 5th Aug 2024 at 1pm is returned as:
+ *   >>> {'2024', '08', '05', '13'}
+ */
+function getCurrentDate() {
+    const currentDate = new Date();
+    const year = String(currentDate.getFullYear());
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hour = String(currentDate.getHours()).padStart(2, '0');
+    return {year, month, day, hour}
+}
+
+/**
+ * Gets the target folder/prefix in the s3 bucket for the required files
+ *
+ * @param {string} target - The mid-prefix / canary name to filter against.
+ * @param {S3Client} s3Client - An optional instance of the S3Client to use for sending the command.
+ * @returns {string} - The relevant prefix for the required files in the s3 bucket
+ */
+async function getRelevantPrefix(target, s3Client = new S3Client()) {
+    const {year, month, day, hour} = getCurrentDate()
+    const key = `canary/eu-west-2/${target}/${year}/${month}/${day}/${hour}`
+    try {
+        const data = await listFiles(S3_CANARY_LOGS_BUCKET_NAME, key)
+
+        const folders = new Set(data.Contents.map(item => {
+            const parts = item.Key.split('/');
+            return parts.slice(0, 9).join('/');
+        }));
+
+        const folderArray = Array.from(folders);
+
+        folderArray.sort((a, b) => (a < b ? 1 : -1));
+        return getDirectoryPath(folderArray[0])
+    } catch (error) {
+        console.error('Error fetching the latest folder:', error);
+        throw error;
+    }
+}
+
+/**
+ * Extracts the keys associated with the failed page snapshots from the given keys
+ *
+ * @param {array} keys - Array of objects representing each of the keys in the s3 folder/prefix
+ * @returns {array} - Arrays of strings representing the keys of the failed page snapshots.
+ */
+function extractScreenshotKeys(keys) {
+    return keys
+        .filter(item => item.Key.includes('-succeeded') && item.Key.endsWith('.png'))
+        .map(item => item.Key);
+}
+
+/**
+ * Extracts the key associated with the `SyntheticsReport` file from the given keys
+ *
+ * @param {array} keys - Array of objects representing each of the keys in the s3 folder/prefix
+ * @returns {string} - The key associated with the `SyntheticsReport` file
+ */
+function extractReportKey(keys) {
+    const reportKeys = keys
+        .filter(item => item.Key.includes('SyntheticsReport'))
+        .map(item => item.Key);
+    return reportKeys[0]
+}
+
+/**
+ * Builds the `blocks` to be sent to the Slack API to post the primary message.
+ *
+ * @param {string} target - The name of the canary from which the alarm was raised.
+ * @param {string} startTime - The time which the canary started its run.
+ * @param {string} endTime - The time which the canary completed its run.
+ *
+ * @returns {array} - An array of JSON objects which can be used to post to the Slack channel with.
+ */
+function buildSlackPostPayload(target, startTime, endTime) {
+    return [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": ":alert: Canary run failed",
+                "emoji": true
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": `*Alarm name:*\n${target}`
+            }
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "plain_text",
+                    "text": `Canary started at ${startTime} and failed at ${endTime}`
+                }
+            ]
+        }
+    ]
+}
+
+/**
+ * Uploads the downloaded file to the given Slack channel under the parent thread as an individual reply.
+ *
+ * @param {object} downloadedFileResponse - The object containing the key of file and the `GetObjectCommandResponse`
+ * @param {WebClient} slackClient - The `WebClient` instance used to interact with Slack.
+ * @param {string} channelId - The ID associated with the channel which the image is being sent to.
+ * @param {string} threadTs - The ID of the parent thread to attach this image as a reply to.
+ *
+ * @returns {Promise} - The promise from the function call
+ */
+async function uploadScreenshotToSlackThread(downloadedFileResponse, slackClient, channelId, threadTs) {
+    const fileBufferStream = await streamToBuffer(downloadedFileResponse.content.Body);
+    const fileName = getFilename(downloadedFileResponse.key);
+    const fileUploadURLResponse = await getFileUploadURLToSlack(slackClient, fileName, fileBuffer.length)
+
+    const uploadURL = fileUploadURLResponse.upload_url
+    const fileID = fileUploadURLResponse.file_id
+
+    await sendPostRequest(uploadURL, fileName, fileBufferStream)
+    const files = [{"id": fileID, "title": fileName}]
+    await completeFileUploadToSlack(slackClient, files, channelId, threadTs)
+}
+
+/**
+ * Uploads the downloaded files to the given Slack channel under the parent thread as an individual reply.
+ *
+ * @param {array} downloadedFileResponses - The objects containing the key of each file and `GetObjectCommandResponse`
+ * @param {WebClient} slackClient - The `WebClient` instance used to interact with Slack.
+ * @param {string} channelId - The ID associated with the channel which the image is being sent to.
+ * @param {string} threadTs - The ID of the parent thread to attach this image as a reply to.
+ *
+ * @returns {Promise} - The promise from the function call
+ */
+async function uploadAllScreenshotsToSlackThread(downloadedFileResponses, slackClient, channelId, threadTs) {
+    for (const downloadedFileResponse of downloadedFileResponses) {
+        await uploadScreenshotToSlackThread(downloadedFileResponse, slackClient, channelId, threadTs)
+    }
+}
+
+/**
+ * Posts a primary/parent message to Slack
+ *
+ * @param {WebClient} slackClient - The `WebClient` instance used to interact with Slack.
+ * @param {array} payload - The `blocks` payload to send with the message.
+ * @param {string} channelId - The ID associated with the channel which the image is being sent to.
+ * @returns {Object} - The response from the underlying `WebClient.chat.postMessage` call.
+ */
+async function sendSlackPost(slackClient, payload, channelId) {
+    return await slackClient.chat.postMessage({
+        token: slackClient.token, channel: channelId, blocks: payload, text: 'Synthetic monitoring alert raised',
+    });
+}
+
+/**
+ * Extracts the contents of the `SyntheticsReport` file from the given `folderContents`
+ *
+ * @param {array} folderContents - Array of objects representing the listed folder contents
+ * @returns {object} - The JSON representation of the contents of the `SyntheticReports` file.
+ */
+async function extractReport(folderContents) {
+    const downloadedReportKey = extractReportKey(folderContents)
+    const downloadedReportResponse = await downloadFile(S3_CANARY_LOGS_BUCKET_NAME, downloadedReportKey,)
+
+    const reportFileBuffer = await streamToBuffer(downloadedReportResponse.Body);
+
+    const jsonString = reportFileBuffer.toString('utf8');
+    return JSON.parse(jsonString);
+}
+
+/**
+ * Extracts the name of the triggered Canary from the `event` object passed to the Lambda runtime.
+ *
+ * @param {object} event - The object passed down to the Lambda runtime on initialization.
+ * @returns {string} - The name of the Canary being triggered.
+ */
+function extractTargetFromEvent(event) {
+    const eventMessage = JSON.parse(event.Records[0].Sns.Message);
+    return eventMessage.Trigger.Dimensions[0].value
+}
+
+/**
+ * Calculates the relevant folder in s3 relating to the triggered Canary results.
+ *
+ * @param {object} event - The object passed down to the Lambda runtime on initialization.
+ * @returns {string} - The prefix associated with the 'folder' of the triggered Canary results.
+ */
+async function determineRelevantFolderInS3(event) {
+    const target = extractTargetFromEvent(event)
+    return getRelevantPrefix(target)
+}
+
+
+/**
+ * Main handler entrypoint for the Lambda runtime execution.
+ *
+ * @param {object} event - The object passed down to the Lambda runtime on initialization.
+ */
+async function handler(event) {
+    const relevantFolder = determineRelevantFolderInS3(event)
+    const slackSecret = await getSlackSecret()
+
+    const slackClient = await buildSlackClient(slackSecret.slack_token)
+
+    const listedFiles = await listFiles(S3_CANARY_LOGS_BUCKET_NAME, relevantFolder)
+    const folderContents = listedFiles.Contents
+
+    const report = await extractReport(folderContents)
+    const slackPayload = buildSlackPostPayload(report.canaryName, report.startTime, report.endTime,)
+    const slackPostResponse = await sendSlackPost(slackClient, slackPayload, slackSecret.slack_channel_id,)
+
+    const extractedSnapshotKeys = extractScreenshotKeys(folderContents)
+    const downloadResponses = downloadAllFiles(extractedSnapshotKeys, S3_CANARY_LOGS_BUCKET_NAME)
+
+    await uploadAllScreenshotsToSlackThread(downloadResponses, slackClient, slackSecret.slack_channel_id, slackPostResponse.ts)
+}
+
+module.exports = {
+    handler
+}
---
+++ terraform/20-app/lambda.canary-notification.tf
@@ -0,0 +1,64 @@
+module "lambda_canary_notification" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "7.7.0"
+  function_name = "${local.prefix}-canary-notification"
+  description   = "Sends notifications when a synthetics canary run fails."
+
+  create_package = true
+  runtime        = "nodejs18.x"
+  handler        = "index.handler"
+  source_path    = "../../src/lambda-canary-notification"
+
+  timeout = 120
+
+  architectures          = ["arm64"]
+  maximum_retry_attempts = 1
+
+  environment_variables = {
+    SECRETS_MANAGER_SLACK_WEBHOOK_URL_ARN = aws_secretsmanager_secret.slack_webhook_url.arn
+    S3_CANARY_LOGS_BUCKET_NAME            = module.s3_canary_logs.s3_bucket_id
+  }
+
+  attach_policy_statements = true
+  policy_statements = {
+    get_screenshots_from_s3_bucket = {
+      effect    = "Allow",
+      actions   = ["s3:GetObject"]
+      resources = ["${module.s3_canary_logs.s3_bucket_arn}/*"]
+    }
+    list_objects_in_s3_bucket = {
+      effect    = "Allow",
+      actions   = ["s3:ListBucket"]
+      resources = [module.s3_canary_logs.s3_bucket_arn]
+    }
+    get_slack_webhook_url_from_secrets_manager = {
+      effect    = "Allow",
+      actions   = ["secretsmanager:GetSecretValue"],
+      resources = [aws_secretsmanager_secret.slack_webhook_url.arn]
+    }
+  }
+
+  create_current_version_allowed_triggers = false
+  allowed_triggers = {
+    sns_cloudfront_alarms = {
+      principal  = "sns.amazonaws.com"
+      source_arn = module.cloudwatch_canary_front_end_screenshots.sns_topic_arn
+    }
+  }
+}
+
+module "lambda_canary_notification_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+
+  name   = "${local.prefix}-lambda-canary-notification"
+  vpc_id = module.vpc.vpc_id
+
+  egress_with_cidr_blocks = [
+    {
+      description = "https to internet"
+      rule        = "https-443-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
