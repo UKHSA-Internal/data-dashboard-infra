cf9a3f33feaa4d61ec910f6fd0588ff32c95f335
A-Ashiq <afaan.ashiq@nate.tech>
2024-08-09 15:13:29 +0100

    Revoke security group rules for cloudwatch canary module security group on delete

--- terraform/modules/cloud-watch-canary/security-group.tf
+++ terraform/modules/cloud-watch-canary/security-group.tf
@@ -5,6 +5,8 @@ module "canary_security_group" {
   name   = var.name
   vpc_id = var.vpc_id
 
+  revoke_rules_on_delete = true
+
   egress_with_cidr_blocks = [
     {
       description = "https to internet"
