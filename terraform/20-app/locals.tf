b11ea02d4138a6ac33ca0057226de39485bd1d63
A-Ashiq <afaan.ashiq@nate.tech>
2024-09-05 16:51:15 +0100

    Capture password rotation event from feature flags db

--- terraform/20-app/locals.tf
+++ terraform/20-app/locals.tf
@@ -69,5 +69,6 @@ locals {
   thirty_days_in_seconds  = 2592000
   five_minutes_in_seconds = 300
 
-  main_db_aurora_password_secret_arn = module.aurora_db_app.cluster_master_user_secret[0]["secret_arn"]
+  main_db_aurora_password_secret_arn          = module.aurora_db_app.cluster_master_user_secret[0]["secret_arn"]
+  feature_flags_db_aurora_password_secret_arn = module.aurora_db_feature_flags.cluster_master_user_secret[0]["secret_arn"]
 }
