e38b0fbc7295f9a33560d138517ed48bc1436103
A-Ashiq <afaan.ashiq@nate.tech>
2024-09-09 07:05:59 +0100

    Delete redundant secret - this now being self-managed by aurora in secrets manager

--- terraform/20-app/passwords.tf
+++ terraform/20-app/passwords.tf
@@ -1,19 +1,3 @@
-resource "random_password" "feature_flags_db_password" {
-  length      = 20
-  min_lower   = 1
-  min_numeric = 1
-  min_upper   = 1
-  special     = false
-}
-
-resource "random_password" "temporary_main_db_credentials" {
-  length      = 20
-  min_lower   = 1
-  min_numeric = 1
-  min_upper   = 1
-  special     = false
-}
-
 resource "random_password" "cms_admin_user_password" {
   length      = 10
   min_numeric = 1
