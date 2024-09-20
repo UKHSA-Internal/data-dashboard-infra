58c1f91f3774f40cd12e366ca0155a64d8d1aaa5
A-Ashiq <afaan.ashiq2@gmail.com>
2024-09-20 11:06:38 +0100

    Replace deprecated mutable-tag based references with immutable-tag based ECR workflows

--- scripts/_lambda.sh
+++ scripts/_lambda.sh
@@ -7,7 +7,6 @@ function _lambda_help() {
     echo "commands:"
     echo "  help                      - this help screen"
     echo 
-    echo "  redeploy-functions        - *DEPRECATED redeploy all the lambda functions"
     echo "  restart-functions         - restarts all the lambda functions, pulling most recent images in the process"
     echo "  logs <env> <lambda name>  - tail logs for the specified lambda"
 
@@ -19,7 +18,6 @@ function _lambda() {
     local args=(${@:2})
 
     case $verb in
-        "redeploy-functions") _lambda_redeploy_functions $args ;;
         "restart-functions") _lambda_restart_functions $args ;;
         "logs") _lambda_logs $args ;;
 
@@ -44,22 +42,6 @@ function _lambda_logs() {
    aws logs tail "/aws/lambda/uhd-${env}-${lambda_name}" --follow
 }
 
-# DEPRECATED
-function _lambda_redeploy_functions() {
-    local ingestion_image_uri=$(_get_ingestion_image_uri)
-    local ingestion_lambda_arn=$(_get_ingestion_lambda_arn)
-
-    echo "Deploying latest image to ingestion lambda..."
-    aws lambda update-function-code \
-        --function-name $ingestion_lambda_arn \
-        --image-uri $ingestion_image_uri \
-        --no-cli-pager \
-        --no-cli-auto-prompt \
-        > /dev/null
-
-    echo "Waiting for lambda to update..."
-    aws lambda wait function-updated-v2 --function-name $ingestion_lambda_arn
-}
 
 function _lambda_restart_functions() {
     local ingestion_image_uri=$(_get_most_recent_ingestion_image_uri)
@@ -87,13 +69,6 @@ function _get_most_recent_ingestion_image_uri() {
     local most_recent_ingestion_image_tag=$(_docker_get_most_recent_image_tag_from_repo ${ingestion_ecr_name})
     local ingestion_image_uri="${ingestion_ecr_url}:${most_recent_ingestion_image_tag}"
     echo ${ingestion_image_uri}
-}
-
-# DEPRECATED
-function _get_ingestion_image_uri() {
-    local terraform_output_file=terraform/20-app/output.json
-    local ingestion_image_uri=$(jq -r '.ecr.value.ingestion_image_uri'  $terraform_output_file)
-    echo $ingestion_image_uri
 }
 
 function _get_ingestion_lambda_arn() {
