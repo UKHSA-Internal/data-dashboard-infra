39095f12aaa93e119aef6c9d3184b18daadd7769
A-Ashiq <afaan.ashiq2@gmail.com>
2024-09-25 10:08:23 +0100

    Remove Cloudwatch rum frontend resources

--- terraform/20-app/cloudwatch.rum.front-end.tf
+++
@@ -1,5 +0,0 @@
-module "cloudwatch_rum_front_end" {
-  source      = "../modules/cloudwatch-rum"
-  name        = "${local.prefix}-front-end-app-monitor"
-  domain_name = local.dns_names.front_end
-}
