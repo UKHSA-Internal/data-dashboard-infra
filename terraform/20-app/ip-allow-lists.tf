94e96b18333c9786abf0d759181d76b9bbe3240b
A-Ashiq <afaan.ashiq2@gmail.com>
2024-10-29 11:13:04 +0000

    Move `Temitope` up to `engineers` group

--- terraform/20-app/ip-allow-lists.tf
+++ terraform/20-app/ip-allow-lists.tf
@@ -16,6 +16,7 @@ locals {
       "165.225.199.78/32",  # Krishna - Windows
       "80.7.227.61/32",     # Kiran
       "92.234.44.48/32",    # Zesh
+      "51.241.222.137/32",  # Temitope Akinsoto
     ],
     project_team = [
       "90.206.168.235/32",  # Debbie
@@ -43,7 +44,6 @@ locals {
       "2.221.74.175/32",    # Gareth
       "81.108.143.100/32",  # Ruairidh Villar
       "90.218.199.1/32",    # Ruth Baxter
-      "51.241.222.137/32",  # Temitope Akinsoto
     ]
   }
   complete_ip_allow_list = tolist(
