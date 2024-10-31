234dd650e54b724192bea26e22c8f1dba619e698
A-Ashiq <afaan.ashiq2@gmail.com>
2024-10-31 10:30:09 +0000

    Add `kms` keys to be used to encrypt app level secrets for operators & engineers

--- terraform/20-app/kms.tf
+++ terraform/20-app/kms.tf
@@ -44,3 +44,35 @@ module "kms_secrets_engineer" {
     aliases                 = ["${local.prefix}-secrets-app-engineer"]
 	aliases_use_name_prefix = true
 }
+
+
+module "kms_secrets_app_operator" {
+	source  = "terraform-aws-modules/kms/aws"
+	version = "3.1.0"
+
+	description             = "Encryption key secrets needed by application operator"
+	key_usage               = "ENCRYPT_DECRYPT"
+	deletion_window_in_days = 7
+    multi_region            = false
+
+	key_owners          	= ["arn:aws:iam::${var.tools_account_id}:root"]
+
+    aliases                 = ["${local.prefix}-secrets-app-operator"]
+	aliases_use_name_prefix = true
+}
+
+
+module "kms_secrets_engineer" {
+	source  = "terraform-aws-modules/kms/aws"
+	version = "3.1.0"
+
+	description             = "Encryption key secrets needed by application engineers"
+	key_usage               = "ENCRYPT_DECRYPT"
+	deletion_window_in_days = 7
+    multi_region            = false
+
+	key_owners          	= ["arn:aws:iam::${var.tools_account_id}:root"]
+
+    aliases                 = ["${local.prefix}-secrets-app-engineer"]
+	aliases_use_name_prefix = true
+}
