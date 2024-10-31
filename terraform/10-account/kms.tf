7b76ce14fa4fe21b39cd60816d49feb840349a7c
A-Ashiq <afaan.ashiq2@gmail.com>
2024-10-31 10:29:29 +0000

    Update KMS key alias

--- terraform/10-account/kms.tf
+++ terraform/10-account/kms.tf
@@ -1,14 +1,14 @@
 module "kms_secrets" {
-	source  = "terraform-aws-modules/kms/aws"
-	version = "3.1.0"
+  source  = "terraform-aws-modules/kms/aws"
+  version = "3.1.0"
 
-	description             = "Account level secrets encryption key"
-	key_usage               = "ENCRYPT_DECRYPT"
-	deletion_window_in_days = 7
-    multi_region            = false
+  description             = "Account level secrets encryption key"
+  key_usage               = "ENCRYPT_DECRYPT"
+  deletion_window_in_days = 7
+  multi_region            = false
 
-	key_owners          	= ["arn:aws:iam::${var.tools_account_id}:root"]
+  key_owners = ["arn:aws:iam::${var.tools_account_id}:root"]
 
-    aliases                 = ["${local.project}}-${local.account}-account-secrets"]
-	aliases_use_name_prefix = true
+  aliases                 = ["${local.project}-${local.account}-account-secrets"]
+  aliases_use_name_prefix = true
 }
