8ce6aecf87d28ecd264ffd02cd8abf4642256be4
A-Ashiq <afaan.ashiq2@gmail.com>
2024-10-31 10:27:55 +0000

    Add `kms` keys to be used to encrypt secrets

---
+++ terraform/10-account/kms.tf
@@ -0,0 +1,14 @@
+module "kms_secrets" {
+	source  = "terraform-aws-modules/kms/aws"
+	version = "3.1.0"
+
+	description             = "Account level secrets encryption key"
+	key_usage               = "ENCRYPT_DECRYPT"
+	deletion_window_in_days = 7
+    multi_region            = false
+
+	key_owners          	= ["arn:aws:iam::${var.tools_account_id}:root"]
+
+    aliases                 = ["${local.project}}-${local.account}-account-secrets"]
+	aliases_use_name_prefix = true
+}
--- terraform/20-app/kms.tf
+++ terraform/20-app/kms.tf
@@ -12,3 +12,35 @@ module "kms_app_rds" {
     aliases                 = ["${local.prefix}-app-rds"]
 	aliases_use_name_prefix = true
 }
+
+
+module "kms_secrets_app_operator" {
+	source  = "terraform-aws-modules/kms/aws"
+	version = "3.1.0"
+
+	description             = "Encryption key secrets needed by application operator"
+	key_usage               = "ENCRYPT_DECRYPT"
+	deletion_window_in_days = 7
+    multi_region            = false
+
+	key_owners          	= ["arn:aws:iam::${var.tools_account_id}:root"]
+
+    aliases                 = ["${local.prefix}-secrets-app-operator"]
+	aliases_use_name_prefix = true
+}
+
+
+module "kms_secrets_engineer" {
+	source  = "terraform-aws-modules/kms/aws"
+	version = "3.1.0"
+
+	description             = "Encryption key secrets needed by application engineers"
+	key_usage               = "ENCRYPT_DECRYPT"
+	deletion_window_in_days = 7
+    multi_region            = false
+
+	key_owners          	= ["arn:aws:iam::${var.tools_account_id}:root"]
+
+    aliases                 = ["${local.prefix}-secrets-app-engineer"]
+	aliases_use_name_prefix = true
+}
