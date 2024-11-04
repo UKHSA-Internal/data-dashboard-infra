33df10a7886cdb732b4bd09b7439692d04384f80
A-Ashiq <afaan.ashiq2@gmail.com>
2024-11-04 11:36:25 +0000

    Provide ECS services with access to required KMS keys

--- terraform/20-app/ecs.service.cms-admin.tf
+++ terraform/20-app/ecs.service.cms-admin.tf
@@ -92,6 +92,17 @@ module "ecs_service_cms_admin" {
     }
   ]
 
+  task_exec_iam_statements = {
+    kms_keys = {
+      actions   = ["kms:Decrypt"]
+      resources = [
+        module.kms_secrets_app_engineer.key_arn,
+        module.kms_app_rds.key_arn,
+        module.kms_secrets_app_operator.key_arn,
+      ]
+    }
+  }
+
   security_group_rules = {
     # ingress rules
     alb_ingress = {
