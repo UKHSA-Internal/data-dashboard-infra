e895f98424ccd90e9fea116d95292384af341d22
A-Ashiq <afaan.ashiq2@gmail.com>
2024-12-06 16:32:43 +0000

    Remove email-based identity for recipient

--- terraform/20-app/ses.feedback-api.tf
+++ terraform/20-app/ses.feedback-api.tf
@@ -5,11 +5,3 @@ resource "aws_ses_domain_identity" "sender" {
 resource "aws_ses_domain_dkim" "sender" {
   domain = aws_ses_domain_identity.sender.domain
 }
-
-data "aws_secretsmanager_secret_version" "private_api_email_credentials" {
-  secret_id = aws_secretsmanager_secret.private_api_email_credentials.id
-}
-
-resource "aws_ses_email_identity" "recipient" {
-  email = jsondecode(data.aws_secretsmanager_secret_version.private_api_email_credentials.secret_string)["feedback_email_recipient_address"]
-}
