334a2a1d2a3986612608ee32676cf8db3773e491
A-Ashiq <afaan.ashiq2@gmail.com>
2024-12-12 15:41:48 +0000

    Add spf verification and dmarc for ses domain-based sender identity

--- terraform/20-app/route-53.tf
+++ terraform/20-app/route-53.tf
@@ -6,72 +6,72 @@ module "route_53_records" {
 
   records = [
     {
-      name  = local.environment
-      type  = "A"
+      name = local.environment
+      type = "A"
       alias = {
         name    = module.cloudfront_front_end.cloudfront_distribution_domain_name
         zone_id = module.cloudfront_front_end.cloudfront_distribution_hosted_zone_id
       }
     },
     {
-      name  = "${local.environment}-lb"
-      type  = "A"
+      name = "${local.environment}-lb"
+      type = "A"
       alias = {
         name    = module.front_end_alb.dns_name
         zone_id = module.front_end_alb.zone_id
       }
     },
     {
-      name  = "${local.environment}-api"
-      type  = "A"
+      name = "${local.environment}-api"
+      type = "A"
       alias = {
         name    = module.cloudfront_public_api.cloudfront_distribution_domain_name
         zone_id = module.cloudfront_public_api.cloudfront_distribution_hosted_zone_id
       }
     },
     {
-      name  = "${local.environment}-api-lb"
-      type  = "A"
+      name = "${local.environment}-api-lb"
+      type = "A"
       alias = {
         name    = module.public_api_alb.dns_name
         zone_id = module.public_api_alb.zone_id
       }
     },
     {
-      name  = "${local.environment}-private-api",
-      type  = "A"
+      name = "${local.environment}-private-api",
+      type = "A"
       alias = {
         name    = module.private_api_alb.dns_name
         zone_id = module.private_api_alb.zone_id
       }
     },
     {
-      name  = "${local.environment}-feedback-api",
-      type  = "A"
+      name = "${local.environment}-feedback-api",
+      type = "A"
       alias = {
         name    = module.feedback_api_alb.dns_name
         zone_id = module.feedback_api_alb.zone_id
       }
     },
     {
-      name  = "${local.environment}-cms"
-      type  = "A"
+      name = "${local.environment}-cms"
+      type = "A"
       alias = {
         name    = module.cms_admin_alb.dns_name
         zone_id = module.cms_admin_alb.zone_id
       }
     },
     {
-      name  = "${local.environment}-archive"
-      type  = "A"
+      name = "${local.environment}-archive"
+      type = "A"
       alias = {
         name    = module.cloudfront_archive_web_content.cloudfront_distribution_domain_name
         zone_id = module.cloudfront_archive_web_content.cloudfront_distribution_hosted_zone_id
       }
     },
     {
-      name  = "${local.environment}-feature-flags"
-      type  = "A"
+      name = "${local.environment}-feature-flags"
+      type = "A"
       alias = {
         name    = module.feature_flags_alb.dns_name
         zone_id = module.feature_flags_alb.zone_id
@@ -84,15 +84,31 @@ resource "aws_route53_record" "ses_verification" {
   zone_id = local.account_layer.dns.account.zone_id
   name    = "_amazonses.${aws_ses_domain_identity.sender.domain}"
   type    = "TXT"
-  ttl     = "300"
+  ttl     = 300
   records = [aws_ses_domain_identity.sender.verification_token]
 }
 
-resource "aws_route53_record" "dkim_record" {
+resource "aws_route53_record" "dkim_records" {
   count   = 3
   zone_id = local.account_layer.dns.account.zone_id
   name    = "${element(aws_ses_domain_dkim.sender.dkim_tokens, count.index)}._domainkey.${aws_ses_domain_identity.sender.domain}"
   type    = "CNAME"
-  ttl     = "300"
+  ttl     = 300
   records = ["${element(aws_ses_domain_dkim.sender.dkim_tokens, count.index)}.dkim.amazonses.com"]
+}
+
+resource "aws_route53_record" "spf" {
+  zone_id = local.account_layer.dns.account.zone_id
+  name    = aws_ses_domain_identity.sender.domain
+  type    = "TXT"
+  ttl     = 300
+  records = ["v=spf1 include:amazonses.com -all"]
+}
+
+resource "aws_route53_record" "dmarc" {
+  zone_id = local.account_layer.dns.account.zone_id
+  name    = "_dmarc.${aws_ses_domain_identity.sender.domain}"
+  type    = "TXT"
+  ttl     = 300
+  records = ["v=DMARC1; p=quarantine; rua=mailto:dmarc-reports@${aws_ses_domain_identity.sender.domain}"]
 }
--- terraform/20-app/ses.feedback-api.tf
+++ terraform/20-app/ses.feedback-api.tf
@@ -2,6 +2,11 @@ resource "aws_ses_domain_identity" "sender" {
   domain = local.dns_names.emails
 }
 
+resource "aws_ses_domain_identity_verification" "sender" {
+  domain     = aws_ses_domain_identity.sender.domain
+  depends_on = [aws_route53_record.ses_verification]
+}
+
 resource "aws_ses_domain_dkim" "sender" {
   domain = aws_ses_domain_identity.sender.domain
 }
