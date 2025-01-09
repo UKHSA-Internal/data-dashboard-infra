74d6fd6de2155707f8c7032c3a01f955db870d65
A-Ashiq <afaan.ashiq2@gmail.com>
2025-01-09 19:58:25 +0000

    Move route53 records for feedback emails into dedicated file

--- terraform/20-app/route-53.tf
+++ terraform/20-app/route-53.tf
@@ -79,36 +79,3 @@ module "route_53_records" {
     }
   ]
 }
-
-resource "aws_route53_record" "ses_verification" {
-  zone_id = local.account_layer.dns.account.zone_id
-  name    = "_amazonses.${aws_ses_domain_identity.sender.domain}"
-  type    = "TXT"
-  ttl     = 300
-  records = [aws_ses_domain_identity.sender.verification_token]
-}
-
-resource "aws_route53_record" "dkim_records" {
-  count   = 3
-  zone_id = local.account_layer.dns.account.zone_id
-  name    = "${element(aws_ses_domain_dkim.sender.dkim_tokens, count.index)}._domainkey.${aws_ses_domain_identity.sender.domain}"
-  type    = "CNAME"
-  ttl     = 300
-  records = ["${element(aws_ses_domain_dkim.sender.dkim_tokens, count.index)}.dkim.amazonses.com"]
-}
-
-resource "aws_route53_record" "spf" {
-  zone_id = local.account_layer.dns.account.zone_id
-  name    = aws_ses_domain_identity.sender.domain
-  type    = "TXT"
-  ttl     = 300
-  records = ["v=spf1 include:amazonses.com -all"]
-}
-
-resource "aws_route53_record" "dmarc" {
-  zone_id = local.account_layer.dns.account.zone_id
-  name    = "_dmarc.${aws_ses_domain_identity.sender.domain}"
-  type    = "TXT"
-  ttl     = 300
-  records = ["v=DMARC1; p=quarantine; rua=mailto:dmarc-reports@${aws_ses_domain_identity.sender.domain}"]
-}
