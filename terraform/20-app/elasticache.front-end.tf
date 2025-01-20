ea0b36af645142ccd5d3f42eadcffbc90862f630
A-Ashiq <afaan.ashiq2@gmail.com>
2025-01-20 14:52:23 +0000

    Hook up serverless elasticache to frontend

---
+++ terraform/20-app/elasticache.front-end.tf
@@ -0,0 +1,25 @@
+module "front_end_elasticache_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+
+  name   = "${local.prefix}-front-end"
+  vpc_id = module.vpc.vpc_id
+
+  ingress_with_source_security_group_id = [
+    {
+      description              = "frontend tasks to cache"
+      rule                     = "redis-tcp"
+      source_security_group_id = module.ecs_service_front_end.security_group_id
+    }
+  ]
+}
+
+resource "aws_elasticache_serverless_cache" "front_end_elasticache" {
+  engine = "redis"
+  name   = "${local.prefix}-front-end-serverless-redis"
+
+  major_engine_version     = "7"
+  snapshot_retention_limit = 1
+  security_group_ids       = [module.front_end_elasticache_security_group.security_group_id]
+  subnet_ids               = module.vpc.elasticache_subnets
+}
