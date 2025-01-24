db9e305bc83cd868413190d9b965537355811d5f
Christian Martin <christian.martin2@nhs.net>
2025-01-24 14:30:29 +0000

    CDD-2443 Add identity providers and SNS code

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -1,6 +1,17 @@
 resource "aws_cognito_user_pool" "user_pool" {
   name = var.user_pool_name
 
+  mfa_configuration = var.enable_mfa ? "ON" : "OFF"
+
+  dynamic "sms_configuration" {
+    for_each = var.enable_sms ? [1] : []
+
+    content {
+      sns_caller_arn = var.sns_role_arn != null ? var.sns_role_arn : ""
+      external_id    = "cognito-sms-external-id"
+    }
+  }
+
   password_policy {
     minimum_length    = 8
     require_lowercase = true
@@ -9,7 +20,7 @@ resource "aws_cognito_user_pool" "user_pool" {
     require_uppercase = true
   }
 
-  auto_verified_attributes = ["email"]
+  auto_verified_attributes = var.enable_sms ? ["email", "phone_number"] : ["email"]
 
   account_recovery_setting {
     recovery_mechanism {
@@ -20,17 +31,22 @@ resource "aws_cognito_user_pool" "user_pool" {
 }
 
 resource "aws_cognito_user_pool_client" "user_pool_client" {
+#  depends_on = [
+#    aws_cognito_identity_provider.oidc_idp,
+#    aws_cognito_identity_provider.saml_idp
+#  ]
+
   name         = var.client_name
   user_pool_id = aws_cognito_user_pool.user_pool.id
 
-  allowed_oauth_flows             = ["code", "implicit"]
+  allowed_oauth_flows                  = ["code", "implicit"]
   allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_scopes            = ["phone", "email", "openid", "profile", "aws.cognito.signin.user.admin"]
+  allowed_oauth_scopes                 = ["phone", "email", "openid", "profile", "aws.cognito.signin.user.admin"]
 
   callback_urls = var.callback_urls
   logout_urls   = var.logout_urls
 
-  supported_identity_providers = ["COGNITO"]
+  supported_identity_providers = ["COGNITO"] # but will support:- ["COGNITO", "NHS", "COBR"]
 }
 
 resource "aws_cognito_user_pool_domain" "user_pool_domain" {
@@ -40,4 +56,40 @@ resource "aws_cognito_user_pool_domain" "user_pool_domain" {
   lifecycle {
     ignore_changes = [domain]
   }
+}
+
+# Commenting out identity providers until details are available
+#resource "aws_cognito_identity_provider" "saml_idp" {
+#  user_pool_id  = aws_cognito_user_pool.user_pool.id
+#  provider_name = "NHS"
+#  provider_type = "SAML"
+
+#  provider_details = {
+#    MetadataURL = var.nhs_metadata_url
+#    IDPSignout   = false
+#  }
+#}
+
+#resource "aws_cognito_identity_provider" "oidc_idp" {
+#  user_pool_id  = aws_cognito_user_pool.user_pool.id
+#  provider_name = "COBR"
+#  provider_type = "OIDC"
+
+#  provider_details = {
+#    authorize_scopes              = "openid email"
+#    client_id                     = var.cobr_oidc_client_id
+#    client_secret                 = var.cobr_oidc_client_secret
+#    oidc_issuer                   = var.cobr_oidc_issuer_url
+#    attributes_request_method     = "GET"
+#    attributes_url                = var.cobr_oidc_attributes_url
+#    attributes_url_add_attributes = "true"
+#  }
+#}
+
+resource "aws_cognito_user_group" "user_groups" {
+  for_each = toset(["Admin", "Analyst", "Viewer"])
+  name          = each.value
+  user_pool_id  = aws_cognito_user_pool.user_pool.id
+  precedence    = lookup(var.group_precedence, each.value, null)
+  description   = "Group for ${each.value} role"
 }
--- terraform/modules/cognito/variables.tf
+++ terraform/modules/cognito/variables.tf
@@ -1,33 +1,94 @@
+variable "region" {
+  description = "The AWS region for resources"
+  type        = string
+}
+
+variable "sns_role_arn" {
+  description = "ARN of the SNS role for MFA"
+  type        = string
+  default     = null
+  validation {
+    condition     = can(regex("^arn:aws:iam::\\d+:role/.+", var.sns_role_arn))
+    error_message = "sns_role_arn must be a valid ARN of an IAM Role."
+  }
+}
+
+variable "nhs_metadata_url" {
+  description = "Metadata URL for NHS SAML IdP"
+  type        = string
+  default     = "https://auth.nhs.gov.uk"
+}
+
+variable "cobr_oidc_client_id" {
+  description = "Client ID for COBR OIDC IdP"
+  type        = string
+}
+
+variable "cobr_oidc_client_secret" {
+  description = "Client secret for COBR OIDC IdP"
+  type        = string
+}
+
+variable "cobr_oidc_issuer_url" {
+  description = "Issuer URL for COBR OIDC IdP"
+  type        = string
+  default     = "https://auth.cobr.gov.uk"
+}
+
+variable "cobr_oidc_attributes_url" {
+  description = "Attributes URL for COBR OIDC IdP"
+  type        = string
+}
+
+variable "callback_urls" {
+  description = "List of allowed callback URLs for OAuth flows"
+  type        = list(string)
+  default     = []
+  validation {
+    condition     = alltrue([for url in var.callback_urls : can(regex("^(http|https)://", url))])
+    error_message = "Each callback URL must start with http:// or https://"
+  }
+}
+
+variable "logout_urls" {
+  description = "List of allowed logout URLs for OAuth flows"
+  type        = list(string)
+  default     = []
+}
+
+variable "group_precedence" {
+  description = "Precedence of user groups"
+  type        = map(number)
+  default     = {
+    Admin   = 1
+    Analyst = 2
+    Viewer  = 3
+  }
+}
+
 variable "user_pool_name" {
-  description = "The name of the Cognito User Pool"
+  description = "Name of the Cognito User Pool"
   type        = string
 }
 
 variable "client_name" {
-  description = "The name of the Cognito User Pool Client"
+  description = "Name of the Cognito User Pool Client"
   type        = string
 }
 
 variable "user_pool_domain" {
-  description = "The domain prefix for Cognito User Pool"
+  description = "Domain for the Cognito User Pool"
   type        = string
-  default     = null
 }
 
-variable "callback_urls" {
-  description = "A list of allowed callback URLs for the User Pool Client"
-  type        = list(string)
-  default     = []
+variable "enable_mfa" {
+  description = "Enable Multi-Factor Authentication (MFA) for Cognito. If true, MFA will be enforced."
+  type        = bool
+  default     = false
 }
 
-variable "logout_urls" {
-  description = "A list of allowed logout URLs for the User Pool Client"
-  type        = list(string)
-  default     = []
-}
-
-variable "tags" {
-  description = "Tags to apply to all resources"
-  type        = map(string)
-  default     = {}
+variable "enable_sms" {
+  description = "Enable SMS functionality for Cognito (e.g. for MFA or auto-verification).. Requires sns_role_arn."
+  type        = bool
+  default     = false
 }
