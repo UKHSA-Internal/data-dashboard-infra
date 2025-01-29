49d33a192ae2c20a0e6825488ef889da0de1a602
Christian Martin <christian.martin2@nhs.net>
2025-01-29 16:28:21 +0000

    CDD-2443 Auto generate secret for cognito user pool

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -44,6 +44,7 @@ resource "aws_cognito_user_pool_client" "user_pool_client" {
 
   name         = var.client_name
   user_pool_id = aws_cognito_user_pool.user_pool.id
+  generate_secret = true
 
   allowed_oauth_flows = ["code", "implicit"]
   allowed_oauth_flows_user_pool_client = true
