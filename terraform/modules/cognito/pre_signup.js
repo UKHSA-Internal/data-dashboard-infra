ebfa102bb2685c1a485ed8b0305cf8da7594a9da
Christian Martin <christian.martin2@nhs.net>
2025-01-30 16:31:29 +0000

    CDD-2443 Auto confirm user and email for debugging

--- terraform/modules/cognito/pre_signup.js
+++ terraform/modules/cognito/pre_signup.js
@@ -1,4 +1,9 @@
 exports.handler = async (event) => {
-    console.log("Pre-signup Lambda invoked with event:", JSON.stringify(event));
+    console.log("Pre-signup Lambda invoked with event:", JSON.stringify(event, null, 2));
+
+    // Auto-confirm user and verify email
+    event.response.autoConfirmUser = true;
+    event.response.autoVerifyEmail = true;
+
     return event;
 };
