e4145d00ccd0f81eb007a4919e06277113f5f44b
Christian Martin <christian.martin2@nhs.net>
2025-01-30 16:45:43 +0000

    CDD-2443 Amend pre signup lambda logging

--- terraform/modules/cognito/pre_signup.js
+++ terraform/modules/cognito/pre_signup.js
@@ -1,9 +1,15 @@
 exports.handler = async (event) => {
     console.log("Pre-signup Lambda invoked with event:", JSON.stringify(event, null, 2));
 
-    // Auto-confirm user and verify email
+    // Check if the email attribute exists before auto-verifying
+    if (event.request.userAttributes && event.request.userAttributes.email) {
+        event.response.autoVerifyEmail = true;
+    } else {
+        console.log("No email provided. Skipping auto-verification.");
+    }
+
+    // auto-confirm the user
     event.response.autoConfirmUser = true;
-    event.response.autoVerifyEmail = true;
 
     return event;
 };
