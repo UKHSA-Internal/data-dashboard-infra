3e7ce748164de73c1bb3e382889209132a196327
Christian Martin <christian.martin2@nhs.net>
2025-02-04 10:41:43 +0000

    CDD-2443 Add additional logging for post auth lambda

--- terraform/modules/cognito/post_auth.js
+++ terraform/modules/cognito/post_auth.js
@@ -1,4 +1,21 @@
 exports.handler = async (event) => {
-    console.log("Post-auth Lambda invoked with event:", JSON.stringify(event));
+    console.log("Post-auth Lambda invoked with event:", JSON.stringify(event, null, 2));
+    console.log("Trigger source:", event.triggerSource);
+    console.log("User Pool ID:", event.userPoolId);
+    console.log("User Name:", event.userName);
+
+    if (event.request.userAttributes) {
+        console.log("User attributes:", JSON.stringify(event.request.userAttributes, null, 2));
+    } else {
+        console.log("No user attributes provided in the event.");
+    }
+
+    console.log("Environment variables:", process.env);
+
+    if (event.triggerSource !== 'PostAuthentication_Authentication') {
+        console.warn("Unexpected trigger source:", event.triggerSource);
+    }
+
+    console.log("Response being returned:", JSON.stringify(event.response, null, 2));
     return event;
 };
