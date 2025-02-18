713fa16d7581cbb77e779803630f764a20dcc40b
Christian Martin <christian.martin2@nhs.net>
2025-02-18 09:56:26 +0000

    CDD-2442 Fix for role key and addition of double quotes

--- scripts/fast-forward-env-branches.sh
+++ scripts/fast-forward-env-branches.sh
@@ -9,27 +9,29 @@ env_branches=("env/uat/uat"
               "env/auth-dev/auth-dev"
               "env/auth-test/auth-test")
 
-for branch in ${env_branches[@]}; do 
-    env=$(echo $branch | cut -d : -f 1 | xargs basename)
-    
-    if git merge-base --is-ancestor origin/$branch main
+for branch in "${env_branches[@]}"; do
+    env=$(basename "$branch" | tr '-' '_')
+
+    git fetch origin "$branch" main
+
+    if git merge-base --is-ancestor origin/"$branch" main
     then
         echo "Fast forward merge is possible to branch $branch"
         echo 
 
-        git checkout $branch
+        git checkout "$branch"
         git merge main --ff-only
         git push
         echo
 
-        if [ $CI ]; then
+        if [ "$CI" ]; then
             echo "deploy_$env=true" >> "$GITHUB_OUTPUT"
         fi
     else
         echo "Fast forward merge is not possible to branch $branch"
         echo
 
-        if [ $CI ]; then
+        if [ "$CI" ]; then
             echo "deploy_$env=false" >> "$GITHUB_OUTPUT"
         fi
     fi
