a581985a3c2b2f3bc44b69811b3f1d37c13fe5b2
Christian Martin <christian.martin2@nhs.net>
2025-03-05 10:05:38 +0000

    CDD-2442 Shorten feature flags and feedback api target groups name

--- terraform/20-app/ecs.service.feedback-api.tf
+++ terraform/20-app/ecs.service.feedback-api.tf
@@ -82,7 +82,7 @@ module "ecs_service_feedback_api" {
 
   load_balancer = {
     service = {
-      target_group_arn = module.feedback_api_alb.target_groups["${local.prefix}-feedback-api-tg"].arn
+      target_group_arn = module.feedback_api_alb.target_groups["${local.prefix}-feedb-api-tg"].arn
       container_name   = "api"
       container_port   = 80
     }
