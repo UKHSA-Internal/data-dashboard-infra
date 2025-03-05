de26d2ecb0da5e0a8c6f445dc7fdc1ad6af8250d
Christian Martin <christian.martin2@nhs.net>
2025-03-05 11:46:02 +0000

    CDD-2442 Readd uhd prefix and revert locals tf files

--- terraform/10-account/locals.tf
+++ terraform/10-account/locals.tf
@@ -1,7 +1,7 @@
 locals {
   region  = "eu-west-2"
   project = "uhd"
-  account = replace(terraform.workspace, "^uhd-", "")
+  account = terraform.workspace
 
   wke_dns_names = {
     dev   = "dev.ukhsa-dashboard.data.gov.uk"
