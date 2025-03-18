165e4f3372525bb30d44721f433987a899c54773
A-Ashiq <afaan.ashiq2@gmail.com>
2025-03-18 10:17:32 +0000

    Allow `GithubInfraCIRole` to push images to ECRs

--- terraform/modules/ecr/ecr.tf
+++ terraform/modules/ecr/ecr.tf
@@ -6,7 +6,10 @@ module "ecr" {
   repository_image_tag_mutability   = "IMMUTABLE"
   repository_name                   = var.name
   repository_read_access_arns       = ["arn:aws:iam::${var.account_id}:root"]
-  repository_read_write_access_arns = ["arn:aws:iam::${var.tools_account_id}:root"]
+  repository_read_write_access_arns = [
+    "arn:aws:iam::${var.tools_account_id}:root",
+    "arn:aws:iam::${var.tools_account_id}:role/GithubInfraCIRole"
+  ]
 
   create_lifecycle_policy     = true
   repository_lifecycle_policy = local.standard_ecr_lifecycle_policy
