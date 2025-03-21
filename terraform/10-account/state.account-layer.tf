f55ff5a34819bb3753c9fec70c15d756633f232e
A-Ashiq <afaan.ashiq2@gmail.com>
2025-03-21 14:17:32 +0000

    Add remote state for `auth_uat` account

--- terraform/10-account/state.account-layer.tf
+++ terraform/10-account/state.account-layer.tf
@@ -63,6 +63,19 @@ data "terraform_remote_state" "uat_account" {
   }
 }
 
+data "terraform_remote_state" "auth_uat_account" {
+  backend = "s3"
+
+  workspace = "auth-uat"
+
+  config = {
+    region         = "eu-west-2"
+    bucket         = "uhd-terraform-states"
+    dynamodb_table = "terraform-state-lock"
+    key            = "account/state.tfstate"
+  }
+}
+
 locals {
   account_states = {
     dev       = data.terraform_remote_state.dev_account.outputs
@@ -70,5 +83,6 @@ locals {
     test      = data.terraform_remote_state.test_account.outputs
     auth_test = data.terraform_remote_state.auth_test_account.outputs
     uat       = data.terraform_remote_state.uat_account.outputs
+    auth_uat  = data.terraform_remote_state.auth_uat_account.outputs
   }
 }
