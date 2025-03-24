8b02fd965d215f671fb9b9d5195ac311e153f2ff
A-Ashiq <afaan.ashiq2@gmail.com>
2025-03-24 16:28:54 +0000

    Add ACM certifcates & DNS records for `auth-perf` env

---
+++ terraform/10-account/acm.cloud-front.wke-auth-test-account.tf
@@ -0,0 +1,20 @@
+module "acm_cloud_front_wke_auth_perf" {
+    source  = "terraform-aws-modules/acm/aws"
+    version = "5.0.1"
+
+    create_certificate = local.account == "auth-test"
+
+    domain_name = local.wke_dns_names.auth-perf
+    zone_id     = local.account == "auth-test" ? module.route_53_zone_wke_test_account.route53_zone_zone_id[local.wke_dns_names.auth-perf] : ""
+
+    providers = {
+        aws = aws.us_east_1
+    }
+
+    subject_alternative_names = [
+        "*.${local.wke_dns_names.auth-perf}"
+    ]
+
+    validation_method   = "DNS"
+    wait_for_validation = true
+}
---
+++ terraform/10-account/acm.wke-auth-test-account.tf
@@ -0,0 +1,16 @@
+module "acm_wke_auth_perf" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "5.0.1"
+
+  create_certificate = local.account == "auth-test"
+
+  domain_name = local.wke_dns_names.auth-perf
+  zone_id     = local.account == "auth-test" ? module.route_53_zone_wke_test_account.route53_zone_zone_id[local.wke_dns_names.auth-perf] : ""
+
+  subject_alternative_names = [
+    "*.${local.wke_dns_names.auth-perf}"
+  ]
+
+  validation_method   = "DNS"
+  wait_for_validation = true
+}
