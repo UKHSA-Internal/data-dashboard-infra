e6414dc3449b748ca2fdfd3147813204846ba3f3
A-Ashiq <afaan.ashiq2@gmail.com>
2025-05-19 11:59:04 +0100

    Remove unused `lambda-api-gateway` module

--- terraform/modules/lambda-api-gateway/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "lambda_function_arn" {
-  description = "The ARN of the deployed Lambda function"
-  value       = aws_lambda_function.api_gateway_lambda.arn
-}
-
-output "lambda_invoke_arn" {
-  description = "The ARN to invoke the Lambda function"
-  value       = aws_lambda_function.api_gateway_lambda.invoke_arn
-}
