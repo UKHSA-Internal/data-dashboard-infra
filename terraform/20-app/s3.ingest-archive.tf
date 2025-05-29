17ba40698bc8fd46ac582c81792d7f6a87389142
A-Ashiq <afaan.ashiq2@gmail.com>
2025-05-29 13:24:36 +0100

    Add ingest archive bucket c/w file versioning & policy to transfer inbound files to glacier storage class

---
+++ terraform/20-app/s3.ingest-archive.tf
@@ -0,0 +1,33 @@
+module "s3_ingest_archive" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.5.0"
+
+  bucket = "${local.s3_ingest_bucket_name}-archive"
+
+  attach_deny_insecure_transport_policy = true
+  force_destroy                         = true
+
+  versioning = {
+    status     = true
+    mfa_delete = false
+  }
+
+  logging = {
+    target_bucket = data.aws_s3_bucket.s3_access_logs.id
+    target_prefix = "${local.s3_ingest_bucket_name}-archive/"
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "archive"
+      enabled = true
+      filter = {
+        prefix = ""
+      }
+      transition = {
+        days          = 0
+        storage_class = "GLACIER_IR"
+      }
+    },
+  ]
+}
