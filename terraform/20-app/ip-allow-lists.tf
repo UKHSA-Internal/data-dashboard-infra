95e163ad43a4222c95baa9c447a44c63a1258474
A-Ashiq <afaan.ashiq2@gmail.com>
2025-09-17 14:00:11 +0100

    Offboard remaining engineering team for end of October

--- terraform/20-app/ip-allow-lists.tf
+++ terraform/20-app/ip-allow-lists.tf
@@ -1,11 +1,6 @@
 locals {
   ip_allow_list = {
     engineers = [
-      "90.250.132.232/32",  # Afaan
-      "154.51.68.102/32",   # Burendo Leeds
-      "167.98.124.170/32",  # Burendo London
-      "90.219.251.228/32",  # Phil
-      "86.9.184.205/32",    # Manu
     ],
     project_team = [
       "77.100.107.252/32",  # Laura
