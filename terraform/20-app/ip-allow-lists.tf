3b1099bb6529fa373394beaad417262f2e82212c
A-Ashiq <afaan.ashiq2@gmail.com>
2025-09-22 16:31:00 +0100

    Remove IP address for Burendo London office from allow list

--- terraform/20-app/ip-allow-lists.tf
+++ terraform/20-app/ip-allow-lists.tf
@@ -3,7 +3,6 @@ locals {
     engineers = [
       "90.250.132.232/32",  # Afaan
       "154.51.68.102/32",   # Burendo Leeds
-      "167.98.124.170/32",  # Burendo London
       "90.219.251.228/32",  # Phil
       "86.9.184.205/32",    # Manu
     ],
