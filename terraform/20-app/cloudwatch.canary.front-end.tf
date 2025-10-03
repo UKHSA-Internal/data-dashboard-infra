0f5db8d98d4d0d9cfde698b405263fa5c77b5d7f
A-Ashiq <afaan.ashiq2@gmail.com>
2025-10-03 14:22:32 +0100

    Prefix given name with `canary` for all corresponding created resources

--- terraform/20-app/cloudwatch.canary.front-end.tf
+++ terraform/20-app/cloudwatch.canary.front-end.tf
@@ -1,6 +1,7 @@
 module "cloudwatch_canary_front_end_screenshots" {
   source = "../modules/cloud-watch-canary"
   name   = "${local.prefix}-front-end"
+  name   = "${local.prefix}-canary-front-end"
 
   vpc_id            = module.vpc.vpc_id
   subnet_ids        = module.vpc.private_subnets
