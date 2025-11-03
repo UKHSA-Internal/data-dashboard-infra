666949ce9cf1794338c593b8a6797c6bccc8551f
A-Ashiq <afaan.ashiq2@gmail.com>
2025-11-03 15:06:00 +0000

    Use `auth-test` account when targeting `auth-perf` or `auth-pen` test environments

--- scripts/_terraform.sh
+++ scripts/_terraform.sh
@@ -587,7 +587,7 @@ function _get_auth_target_aws_account_name() {
     if [[ $workspace == "auth-prod" ]]; then
         echo "auth-prod"
     elif [[ $CI == "true" ]]; then
-        if [[ $workspace == ci-* ]]; then
+        if [[ $workspace == ci-* ]]|| [[ $workspace == perf ]] || [[ $workspace == pen ]]; then
             echo "auth-test"
         else
             case $branch in
