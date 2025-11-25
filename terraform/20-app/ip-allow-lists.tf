e54ed27058b1b8164b1b496fdf79db4c23ac2c33
kmahmood10 <khawar.mahmoood@ukhsa.gov.uk>
2025-11-25 13:09:19 +0000

    update Jayne Gilberts ip

--- terraform/20-app/ip-allow-lists.tf
+++ terraform/20-app/ip-allow-lists.tf
@@ -34,7 +34,7 @@ locals {
       "165.225.17.43/32",   # Maria Tsiko
       "136.226.167.91/32",  # Emmanuel Ughoo
       "147.161.224.180/32", # Osazee Ogunje
-      "147.161.236.80/32",  # Jayne Gilbert
+      "165.225.199.111/32", # Jayne Gilbert
     ]
     pen_testers = [
       "82.68.136.38/32",    # Steve Ryan
