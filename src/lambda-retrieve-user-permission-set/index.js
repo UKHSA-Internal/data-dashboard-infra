0168de9c8c0c8a05e4cccebbe63468d3f9261c12
Kathryn Dale <kathryn.dale@burendo.com>
2026-05-07 15:42:40 +0100

    Add some fun logs

--- src/lambda-retrieve-user-permission-set/index.js
+++ src/lambda-retrieve-user-permission-set/index.js
@@ -15,12 +15,17 @@ async function getSecretApiKey() {
 
 
 async function getPermissionSets(apiKey, userId) {
+    console.log("🕵️‍♀️ Inside getPermissionSets. UserID: ", userId)
     const baseURL = process.env.PRIVATE_API_URL;
     const targetURL = new URL(`/api/user/${userId}/permissions/hierarchy`, baseURL);
     const headers = { Authorization: apiKey,  'content-type': 'application/json' };
     const response = await fetch(targetURL, {method: 'GET', headers})
+    console.log("🕵️‍♀️ Raw Response: ", response)
+    console.log("🕵️‍♀️ json Response: ", response.json)
     const data = await response.json()
+    console.log("Data: ", data)
     const { permission_sets } = data;
+    console.log("🕵️‍♀️ Permission_Sets: ", permission_sets)
     return permission_sets;
 }
 
