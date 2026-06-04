35ec0016cba82a142438ae323476e51bab36555a
itsthatianguy <ian.j.rufus@gmail.com>
2026-06-04 16:18:09 +0100

    This is a terrible idea

--- terraform/20-app/cloudwatch.subscription-filters.tf
+++ terraform/20-app/cloudwatch.subscription-filters.tf
@@ -1,3 +1,23 @@
+resource "time_sleep" "wait_for_public_api_log_group" {
+  depends_on      = [module.ecs_service_public_api]
+  create_duration = "15s"
+}
+
+resource "time_sleep" "wait_for_private_api_log_group" {
+  depends_on      = [module.ecs_service_private_api]
+  create_duration = "15s"
+}
+
+resource "time_sleep" "wait_for_front_end_log_group" {
+  depends_on      = [module.ecs_service_front_end]
+  create_duration = "15s"
+}
+
+resource "time_sleep" "wait_for_cms_admin_log_group" {
+  depends_on      = [module.ecs_service_cms_admin]
+  create_duration = "15s"
+}
+
 resource "aws_cloudwatch_log_subscription_filter" "ecs_public_api_audit_filter" {
   name            = "${local.prefix}-audit-log-filter-public-api"
   log_group_name  = module.ecs_service_public_api.container_definitions.api.cloudwatch_log_group_name
@@ -5,7 +25,7 @@ resource "aws_cloudwatch_log_subscription_filter" "ecs_public_api_audit_filter" 
   destination_arn = aws_kinesis_firehose_delivery_stream.audit_stream.arn
   role_arn        = aws_iam_role.cloudwatch_to_firehose_role.arn
 
-  depends_on = [module.ecs_service_public_api]
+  depends_on = [time_sleep.wait_for_public_api_log_group]
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "ecs_private_api_audit_filter" {
@@ -15,7 +35,7 @@ resource "aws_cloudwatch_log_subscription_filter" "ecs_private_api_audit_filter"
   destination_arn = aws_kinesis_firehose_delivery_stream.audit_stream.arn
   role_arn        = aws_iam_role.cloudwatch_to_firehose_role.arn
 
-  depends_on = [module.ecs_service_private_api]
+  depends_on = [time_sleep.wait_for_private_api_log_group]
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "ecs_frontend_audit_filter" {
@@ -25,7 +45,7 @@ resource "aws_cloudwatch_log_subscription_filter" "ecs_frontend_audit_filter" {
   destination_arn = aws_kinesis_firehose_delivery_stream.audit_stream.arn
   role_arn        = aws_iam_role.cloudwatch_to_firehose_role.arn
 
-  depends_on = [module.ecs_service_front_end]
+  depends_on = [time_sleep.wait_for_front_end_log_group]
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "ecs_cms_admin_audit_filter" {
@@ -35,5 +55,5 @@ resource "aws_cloudwatch_log_subscription_filter" "ecs_cms_admin_audit_filter" {
   destination_arn = aws_kinesis_firehose_delivery_stream.audit_stream.arn
   role_arn        = aws_iam_role.cloudwatch_to_firehose_role.arn
 
-  depends_on = [module.ecs_service_cms_admin]
+  depends_on = [time_sleep.wait_for_cms_admin_log_group]
 }
