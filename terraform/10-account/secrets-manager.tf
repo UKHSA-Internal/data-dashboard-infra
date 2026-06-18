6d233365b84e8e7e32042ca01267a55f1504b187
itsthatianguy <ian.j.rufus@gmail.com>
2026-06-18 13:27:18 +0100

    Consolidate entra secrets

--- terraform/10-account/secrets-manager.tf
+++ terraform/10-account/secrets-manager.tf
@@ -3,17 +3,7 @@ resource "aws_secretsmanager_secret" "sentinel_external_id" {
   kms_key_id = module.kms_secrets.key_id
 }
 
-resource "aws_secretsmanager_secret" "entra_audience" {
-  name       = "entra/audience"
-  kms_key_id = module.kms_secrets.key_id
-}
-
-resource "aws_secretsmanager_secret" "entra_app_id" {
-  name       = "entra/app-id"
-  kms_key_id = module.kms_secrets.key_id
-}
-
-resource "aws_secretsmanager_secret" "entra_tenant_id" {
-  name       = "entra/tenant-id"
+resource "aws_secretsmanager_secret" "entra" {
+  name       = "entra"
   kms_key_id = module.kms_secrets.key_id
 }
