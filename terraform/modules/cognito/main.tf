e79c9986adbe0a4ec51371e1ecf1718cc1a8a51d
Josh Humphries <josh.humphries@ukhsa.gov.uk>
2026-09-22 16:19:57 +0100

    fix: remove custom groups from attr mapping
    We aren't using the user's groups any more so this is unecessary and causing
    and issue with size limits when the user is in a lot of groups.

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -151,7 +151,6 @@ resource "aws_cognito_identity_provider" "ukhsa_oidc_idp" {
   }
 
   attribute_mapping = {
-    "custom:groups" = "groups"
     "custom:entraObjectId" = "oid"
     "name"          = "name"
     "username"      = "sub"
